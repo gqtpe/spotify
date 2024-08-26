77ace248a4e8b20df53601b196f9002a696217f0
gqtpe <work.temirtas@gmail.com>
2024-08-26 21:06:01 +0500

    search

--- src/features/Browse/Search/Search.tsx
+++ src/features/Browse/Search/Search.tsx
@@ -1,4 +1,4 @@
-import {DetailedHTMLProps, InputHTMLAttributes, memo} from "react";
+import {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, memo, useEffect, useState} from "react";
 import styles from "./Search.module.scss";
 import {IoIosSearch} from "react-icons/io";
 import {useNavigate} from "react-router-dom";
@@ -8,7 +8,21 @@ type Props = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputE
     width: number;
 }
 const Search = ({width, placeholder = 'What do you want to play?', ...rest}:Props) => {
+    //todo:bind with rtk
+
+    // const [value, setValue] = useState('')
+    // const [items, setItems] = useState<any>([])
+    // const [filteredItems, setFilteredItems] = useState<Todo[]>(items)
+    // const [timeoutId, setTimoutId] = useState<number|null>(0)
+
     const navigate = useNavigate()
+    // useEffect(()=>{
+    //
+    // },[])
+    //
+    // const onChange = (e:ChangeEvent<HTMLInputElement>) =>{
+    //
+    // }
 
     return <div className={styles.search} style={{width: `${width*16}px`}}>
         <IoIosSearch id="search-icon" size={30}/>
