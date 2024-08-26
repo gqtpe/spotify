b68d55a5b872c64b56134929e6bb306187b80310
gqtpe <work.temirtas@gmail.com>
2024-08-26 21:14:54 +0500

    Add TabGroup to Browse component for category filtering

--- src/features/Browse/Browse.tsx
+++ src/features/Browse/Browse.tsx
@@ -1,9 +1,17 @@
-
-
+import TabItem from "../../common/components/TabGroup/TabItem/TabItem.tsx";
+import TabGroup from "../../common/components/TabGroup/TabGroup.tsx";
+import {useState} from "react";
 
 
 export const Browse = () =>{
+    const [activeTab, setActiveTab] = useState('all');
     return <div>
-        <h1>Browse</h1>
+        <TabGroup value={activeTab} onChange={setActiveTab}>
+            <TabItem value="all" label="All"/>
+            <TabItem value="songs" label="Songs"/>
+            <TabItem value="playlists" label="Playlists"/>
+            <TabItem value="albums" label="Albums"/>
+            <TabItem value="artists" label="Artists"/>
+        </TabGroup>
     </div>
 }
