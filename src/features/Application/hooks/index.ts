3c26ccb2f4b58ac4d9528ee482f2fa512b8c6766
gqtpe <work.temirtas@gmail.com>
2024-08-26 21:13:16 +0500

    Create index.ts for centralizing app hooks

---
+++ src/features/Application/hooks/index.ts
@@ -0,0 +1,11 @@
+import { useActions } from "./useActions.ts";
+import { useAppDispatch } from "./useAppDispatch.ts";
+import { useAppSelector } from "./useAppSelector.ts";
+
+
+
+export {
+    useAppDispatch,
+    useAppSelector,
+    useActions,
+}
