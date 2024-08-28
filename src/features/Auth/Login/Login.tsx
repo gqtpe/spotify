9494f75f611a50f6d20ba165b69378d2fecede82
gqtpe <work.temirtas@gmail.com>
2024-08-28 20:38:22 +0500

    spotify v1(switched to authorization code flow):switch to authorization code flow and update auth URL

--- src/features/Auth/Login/Login.tsx
+++ src/features/Auth/Login/Login.tsx
@@ -1,20 +1,18 @@
-import {Navigate} from "react-router-dom";
-import {useLogin} from "./useLogin.ts";
 import Button from "../../../common/components/Button/Button.tsx";
+import {scopes} from "../../../api/scopes.ts";
 
-
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const RESPONSE_TYPE = import.meta.env.VITE_RESPONSE_TYPE;
+const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+const authEndpoint = import.meta.env.VITE_AUTH_ENDPOINT;
 
 const Login = () => {
     console.log('Login')
-    const {loggedIn, fromPage, authURL} = useLogin()
-    if (loggedIn) {
-        return <Navigate to={fromPage} replace/>
-    }
+    const endpoint = `${authEndpoint}/?response_type=${RESPONSE_TYPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes}&show_dialog=true`
+
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '12px 0'}}>
-            <Button variant="filled"
-                    link={authURL}>Log
-                In</Button>
+            <Button variant="filled" link={endpoint}>Log In</Button>
         </div>
     );
 };
