0d6c754117d2bfc82ce24e6cd7fc58de1ee2158a
gqtpe <work.temirtas@gmail.com>
2024-08-28 20:56:16 +0500

    spotify v1(switched to authorization code flow): update authSlice to handle Spotify token and user data with async actions

--- src/features/Auth/authSlice.ts
+++ src/features/Auth/authSlice.ts
@@ -1,32 +1,56 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {spotifyAPI, User} from "../../api/spotifyAPI.ts";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {SpotifyTokenResponse, spotifyTokenService, User} from "../../api/spotifyAPI.ts";
 import {initializeApp} from "../Application/appSlice.ts";
+import {setItem} from "../../utils/localStorage.ts";
 
 export type Token = string
-// type StateType = {
-//     loggedIn: boolean
-//     authToken: Token | null
-//     user: null | User
-// }
 
+export const fetchSpotifyToken = createAsyncThunk<SpotifyTokenResponse, string>('auth/fetchSpotifyToken', async (code, {rejectWithValue}) => {
+    try {
+        const response = await spotifyTokenService.getToken(code)
+        console.log(response.data)
+        return response.data
+    } catch (e) {
+        return rejectWithValue(e)
+    }
+})
+
+
+export const asyncActions = {
+    fetchSpotifyToken
+}
 
 export const slice = createSlice({
     name: 'auth',
     initialState: {
         loggedIn: false as boolean,
         authToken: null as Token | null,
-        user: null as null | User,
+        refreshToken: null as Token | null,
+        expirationTime: null as number | null,
+        user: null as null | User
     },
-    reducers: {
-        setToken(state, action: PayloadAction<Token>) {
-            state.authToken = action.payload
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(initializeApp.fulfilled, (state, action: PayloadAction<SpotifyTokenResponse|null>) => {
+            if (state.authToken) {
+                state.authToken = action.payload!.access_token
+                state.refreshToken = action.payload!.refresh_token
+                state.expirationTime = action.payload!.expires_in
+                state.loggedIn = true
+            }
+        })
+        builder.addCase(fetchSpotifyToken.fulfilled, (state, action: PayloadAction<SpotifyTokenResponse>) => {
+            const {access_token, refresh_token, expires_in} = action.payload;
+            state.authToken = access_token
+            state.refreshToken = refresh_token
             state.loggedIn = true
-            spotifyAPI._setToken(action.payload)
-        }
-    },
-    extraReducers: (builder) => {
-        builder.addCase(initializeApp.fulfilled, (state, action: PayloadAction<User>) => {
-            state.user = action.payload
+            state.expirationTime = Date.now() + expires_in * 1000
+
+            setItem('auth_token', access_token);
+            setItem('refresh_token', refresh_token);
+            setItem('expiration_time', (Date.now() + expires_in * 1000).toString());
+            setItem('token_type', action.payload.token_type);
+            setItem('scope', action.payload.scope);
         })
     }
 })
