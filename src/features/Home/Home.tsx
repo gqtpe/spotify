2f4500fa3502e6385621a713dd86db5b86748115
gqtpe <work.temirtas@gmail.com>
2024-08-28 20:51:08 +0500

    spotify v1(switched to authorization code flow): test is token correctly bind

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,6 +1,14 @@
+import Button from "../../common/components/Button/Button.tsx";
+import {spotifyAPI} from "../../api/spotifyAPI.ts";
 
 export const Home = () => {
+
+    const handleClick = () =>{
+        const data = spotifyAPI.getMe().then(res=>res.data)
+        console.log(data)
+    }
     return <div>
         <h1>Home</h1>
+        <Button onClick={handleClick}>get me</Button>
     </div>
 }
