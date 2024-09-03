41c4895303d70b43756d2bfa3fef2e1fdab9793c
gqtpe <work.temirtas@gmail.com>
2024-09-03 10:47:21 +0500

    spotify v1(search): added nested route

--- src/features/Browse/Browse.tsx
+++ src/features/Browse/Browse.tsx
@@ -1,17 +1,39 @@
 import TabItem from "../../common/components/TabGroup/TabItem/TabItem.tsx";
 import TabGroup from "../../common/components/TabGroup/TabGroup.tsx";
-import {useState} from "react";
+import {appHooks} from "../Application";
+import {useActions} from "../Application/hooks";
+import {browseActions} from "./index.ts";
+import {Outlet, useLocation} from "react-router-dom";
+import {tabs} from "./tabs.ts";
+import {useEffect} from "react";
+import {Tabs} from "./browseSlice.ts";
 
 
-export const Browse = () =>{
-    const [activeTab, setActiveTab] = useState('all');
+export const Browse = () => {
+    const activeTab = appHooks.useAppSelector(state => state.browse.activeTab)
+    const {setActiveTab} = useActions(browseActions)
+    const location = useLocation()
+    const tabItems = tabs.map((tab) => {
+            if(tab === 'all') return <TabItem key={tab} value={tab} label={'All'}/>
+            tab = tab + 's'
+            return <TabItem key={tab} value={tab} label={tab[0].toUpperCase() + tab.slice(1)}/>
+        })
+    useEffect(()=>{
+        const tab = location.pathname.split('/')[2] as Tabs
+        if(!tab){
+            setActiveTab('all')
+        }else{
+            setActiveTab(tab)
+        }
+
+    },[location.pathname, setActiveTab])
     return <div>
-        <TabGroup value={activeTab} onChange={setActiveTab}>
-            <TabItem value="all" label="All"/>
-            <TabItem value="songs" label="Songs"/>
-            <TabItem value="playlists" label="Playlists"/>
-            <TabItem value="albums" label="Albums"/>
-            <TabItem value="artists" label="Artists"/>
+        <TabGroup value={activeTab} onChange={(e) => setActiveTab(e)}>
+            {tabItems}
         </TabGroup>
+        <Outlet/>
     </div>
 }
+
+
+
