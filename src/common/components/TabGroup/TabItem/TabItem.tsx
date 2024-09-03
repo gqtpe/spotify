43724e87dd2920e88129c024b79120417c399f34
gqtpe <work.temirtas@gmail.com>
2024-09-03 10:50:58 +0500

    spotify v1(search): refactoring

--- src/common/components/TabGroup/TabItem/TabItem.tsx
+++ src/common/components/TabGroup/TabItem/TabItem.tsx
@@ -1,18 +1,17 @@
 import {DetailedHTMLProps, HTMLAttributes, memo} from "react";
 import styles from '../TabGroup.module.scss'
 
-export type TabItemProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> &{
+export type TabItemProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
     value: string
     label: string
     active?: boolean
 }
-const TabItem = ({value,label,active, ...rest}: TabItemProps) =>{
-    console.log(value)
+const TabItem = ({label, active, ...rest}: TabItemProps) => {
 
 
-   return <div className={[styles.tabItem, rest.className,active?styles.tabItem__active:''].join(' ')} {...rest}>
-       {label}
-   </div>
+    return <div className={[styles.tabItem, rest.className, active ? styles.tabItem__active : ''].join(' ')} {...rest}>
+        {label}
+    </div>
 }
 
 
