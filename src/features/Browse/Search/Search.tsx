a2ecc85b263014571921e3c96f914b96222df27e
gqtpe <work.temirtas@gmail.com>
2024-09-03 10:47:44 +0500

    spotify v1(search): bind search with rtk done

--- src/features/Browse/Search/Search.tsx
+++ src/features/Browse/Search/Search.tsx
@@ -1,38 +1,27 @@
 import {DetailedHTMLProps, InputHTMLAttributes, memo} from "react";
 import styles from "./Search.module.scss";
 import {IoIosSearch} from "react-icons/io";
-import {useNavigate} from "react-router-dom";
-
+import {useSearch} from "./useSearch.ts";
 
 type Props = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
     width: number;
 }
+
 const Search = ({width, placeholder = 'What do you want to play?', ...rest}: Props) => {
-    //todo:bind with rtk
-
-    // const [value, setValue] = useState('')
-    // const [items, setItems] = useState<any>([])
-    // const [filteredItems, setFilteredItems] = useState<Todo[]>(items)
-    // const [timeoutId, setTimoutId] = useState<number|null>(0)
-
-    const navigate = useNavigate()
-    // useEffect(()=>{
-    //
-    // },[])
-    //
-    // const onChange = (e:ChangeEvent<HTMLInputElement>) =>{
-    //
-    // }
-
-    return <div className={styles.search} style={{width: `${width * 16}px`}}>
-        <IoIosSearch id="search-icon" size={30}/>
-        <input
-            type="text"
-            onFocus={() => navigate('/search')}
-            placeholder={placeholder}
-            {...rest}
-        />
-    </div>
+    const {onChange,onFocus,value} = useSearch();
+    return (
+        <div className={styles.search} style={{width: `${width * 16}px`}}>
+            <IoIosSearch id="search-icon" size={30}/>
+            <input
+                type="text"
+                onFocus={onFocus}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                {...rest}
+            />
+        </div>
+    )
 }
 
 export default memo(Search)
