6072cc5b177d305931835e9e765f75e5c3a09462
gqtpe <work.temirtas@gmail.com>
2024-09-05 09:25:58 +0500

    search(query synchronization with the address bar):refactoring

--- src/features/Browse/Browse.tsx
+++ src/features/Browse/Browse.tsx
@@ -1,39 +1,45 @@
+import {Outlet, useLocation} from "react-router-dom";
+import {useCallback, useEffect} from "react";
+import {Tabs} from "./browseSlice.ts";
+import {tabs} from "./tabs.ts";
 import TabItem from "../../common/components/TabGroup/TabItem/TabItem.tsx";
 import TabGroup from "../../common/components/TabGroup/TabGroup.tsx";
 import {appHooks} from "../Application";
-import {useActions} from "../Application/hooks";
 import {browseActions} from "./index.ts";
-import {Outlet, useLocation} from "react-router-dom";
-import {tabs} from "./tabs.ts";
-import {useEffect} from "react";
-import {Tabs} from "./browseSlice.ts";
 
 
 export const Browse = () => {
-    const activeTab = appHooks.useAppSelector(state => state.browse.activeTab)
+    const {useAppSelector, useActions} = appHooks
+    const activeTab = useAppSelector(state => state.browse.activeTab)
     const {setActiveTab} = useActions(browseActions)
     const location = useLocation()
-    const tabItems = tabs.map((tab) => {
-            if(tab === 'all') return <TabItem key={tab} value={tab} label={'All'}/>
 
-            return <TabItem key={tab} value={tab} label={tab[0].toUpperCase() + tab.slice(1) + 's'}/>
-        })
-    useEffect(()=>{
-        const tab = location.pathname.split('/')[2] as Tabs
-        if(!tab){
+    const handleChange = useCallback((tab: Tabs) => {
+        setActiveTab(tab)
+    }, [setActiveTab])
+
+
+    const tabItems = tabs.map((tab) => {
+        if (tab === 'all') return <TabItem key={tab} value={tab} label={'All'}/>
+
+        return <TabItem key={tab} value={tab} label={tab[0].toUpperCase() + tab.slice(1) + 's'}/>
+    })
+
+
+    useEffect(() => {
+        //did mount
+        const tab = location.pathname.split('/')[3] as Tabs
+        if (!tab) {
             setActiveTab('all')
-        }else{
+        } else {
             setActiveTab(tab)
         }
+    }, [])
 
-    },[location.pathname, setActiveTab])
     return <div>
-        <TabGroup value={activeTab} onChange={(e) => setActiveTab(e)}>
+        <TabGroup value={activeTab} onChange={handleChange}>
             {tabItems}
         </TabGroup>
         <Outlet/>
     </div>
 }
-
-
-
