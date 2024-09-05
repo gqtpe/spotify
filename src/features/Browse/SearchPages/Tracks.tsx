e1938fc374f4f6e4ac294428966543e937c2956c
gqtpe <work.temirtas@gmail.com>
2024-09-05 09:27:12 +0500

    search(query synchronization with the address bar):refactoring

--- src/features/Browse/SearchPages/Tracks.tsx
+++ src/features/Browse/SearchPages/Tracks.tsx
@@ -1,104 +1,40 @@
 import {memo, useMemo} from "react";
-import {MantineReactTable, type MRT_ColumnDef, MRT_Table, useMantineReactTable} from "mantine-react-table";
+import {appHooks} from "../../Application";
+import {type MRT_ColumnDef, MRT_Table, useMantineReactTable} from "mantine-react-table";
+import {Track} from "../../../api/spotifyAPI.ts";
 
-type Person = {
-    name: {
-        firstName: string;
-        lastName: string;
-    };
-    address: string;
-    city: string;
-    state: string;
-};
-
-//nested data is ok, see accessorKeys in ColumnDef below
-const data: Person[] = [
-    {
-        name: {
-            firstName: 'Zachary',
-            lastName: 'Davis',
-        },
-        address: '261 Battle Ford',
-        city: 'Columbus',
-        state: 'Ohio',
-    },
-    {
-        name: {
-            firstName: 'Robert',
-            lastName: 'Smith',
-        },
-        address: '566 Brakus Inlet',
-        city: 'Westerville',
-        state: 'West Virginia',
-    },
-    {
-        name: {
-            firstName: 'Kevin',
-            lastName: 'Yan',
-        },
-        address: '7777 Kuhic Knoll',
-        city: 'South Linda',
-        state: 'West Virginia',
-    },
-    {
-        name: {
-            firstName: 'John',
-            lastName: 'Upton',
-        },
-        address: '722 Emie Stream',
-        city: 'Huntington',
-        state: 'Washington',
-    },
-    {
-        name: {
-            firstName: 'Nathan',
-            lastName: 'Harris',
-        },
-        address: '1 Kuhic Knoll',
-        city: 'Ohiowa',
-        state: 'Nebraska',
-    },
-];
 
 const Tracks = () => {
-    // const tracks = useAppSelector(state=>state.browse.items.tracks)
-    // console.log(tracks)
-    // if(!tracks || tracks.items.length === 0) {return <p>(no tracks)</p>}
-    //
-    //
-    // const columns = useMemo<MRT_ColumnDef<Person>[]>(
-    //     () => [
-    //         {
-    //             accessorKey: 'name.firstName', //access nested data with dot notation
-    //             header: 'First Name',
-    //         },
-    //         {
-    //             accessorKey: 'name.lastName',
-    //             header: 'Last Name',
-    //         },
-    //         {
-    //             accessorKey: 'address', //normal accessorKey
-    //             header: 'Address',
-    //         },
-    //         {
-    //             accessorKey: 'city',
-    //             header: 'City',
-    //         },
-    //         {
-    //             accessorKey: 'state',
-    //             header: 'State',
-    //         },
-    //     ],
-    //     [],
-    // );
+    const {useAppSelector} = appHooks
+    const tracks = useAppSelector(state => state.browse.items.tracks)
+
+    const columns = useMemo<MRT_ColumnDef<Track>[]>(() => [
+        {
+            accessorKey: 'name',
+            header: 'Title',
+        },
+        {
+            accessorKey: 'album',
+            header: 'Album',
+        },
+        {
+            accessorKey: 'duration_ms',
+            header: 'Duration',
+        }
+    ], [])
+    const table = useMantineReactTable({
+        columns: columns,
+        data: tracks.items
+    })
+    console.log(tracks)
+    if (!tracks || tracks.items.length === 0) {
+        return <p>(no tracks)</p>
+    }
     //todo: add mantine tables
     return (
         <div>
             <h1>Tracks</h1>
-            {/*{*/}
-            {/*    tracks.items.map(track => <div key={track.id}>{track.name}</div>)*/}
-            {/*}*/}
-            {/*<MRT_Table table={table} />;;*/}
+            <MRT_Table table={table}/>
         </div>
     );
 };
