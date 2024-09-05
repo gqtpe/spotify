95580ce2e197790f4101aee734eb6de113a303fc
gqtpe <work.temirtas@gmail.com>
2024-09-05 09:25:28 +0500

    search(query synchronization with the address bar):added query param, add children

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import {store} from "./app/store.ts";
 import {createRoot} from 'react-dom/client'
-import App from './app/App.tsx'
-import './index.scss'
-import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
-import {Login} from "./features/Auth";
 import {Provider} from "react-redux";
+import {store} from "./app/store.ts";
+import {createBrowserRouter, Navigate, Outlet, RouterProvider} from "react-router-dom";
+import App from './app/App.tsx'
+import {Login} from "./features/Auth";
 import {RequireAuth} from "./common/hoc/RequireAuth.tsx";
 import {Home} from "./features/Home/Home.tsx";
-import {Browse} from "./features/Browse/Browse.tsx";
 import Callback from "./features/callback/Callback.tsx";
-import Tracks from "./features/Browse/SearchPages/Tracks.tsx";
 import {createTheme, MantineProvider} from "@mantine/core";
+import {Browse} from "./features/Browse/Browse.tsx";
+import './index.scss'
 
 
 const router = createBrowserRouter([
@@ -28,28 +27,39 @@ const router = createBrowserRouter([
             },
             {
                 path: '/search',
-                element: <RequireAuth><Browse/></RequireAuth>,
+                element: <RequireAuth><Outlet/></RequireAuth>,
                 children: [
                     {
                         index: true,
-                        element: <div>all</div>
+                        element: <div>Start searching please BOOOOM</div>
                     },
                     {
-                        path: '/search/track',
-                        element: <Tracks/>
-                    },
-                    {
-                        path: '/search/artist',
-                        element: <div>artists</div>
-                    },
-                    {
-                        path: '/search/album',
-                        element: <div>albums</div>
-                    },
-                    {
-                        path: '/search/playlist',
-                        element: <div>playlists</div>
+                        path: '/search/:query',
+                        element: <Browse/>,
+                        children: [
+                            {
+                                index: true,
+                                element: <div>all</div>
+                            },
+                            {
+                                path: '/search/:query/track',
+                                element: <div>tracks</div>
+                            },
+                            {
+                                path: '/search/:query/artist',
+                                element: <div>artists</div>
+                            },
+                            {
+                                path: '/search/:query/album',
+                                element: <div>albums</div>
+                            },
+                            {
+                                path: '/search/:query/playlist',
+                                element: <div>playlists</div>
+                            }
+                        ]
                     }
+
                 ]
             }
         ]
@@ -70,7 +80,7 @@ const theme = createTheme({});
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <MantineProvider theme={theme}>
-        <RouterProvider router={router}/>
+            <RouterProvider router={router}/>
         </MantineProvider>
     </Provider>
 )
