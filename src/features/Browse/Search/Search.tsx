c460ed987d71ebb83f759f6875507ec0e720eb11
gqtpe <work.temirtas@gmail.com>
2024-09-07 08:45:47 +0500

    search: moved debounce logic out of Search

--- src/features/Browse/Search/Search.tsx
+++ src/features/Browse/Search/Search.tsx
@@ -1,23 +1,18 @@
 import {DetailedHTMLProps, InputHTMLAttributes, memo} from "react";
 import styles from "./Search.module.scss";
 import {IoIosSearch} from "react-icons/io";
-import {useSearch} from "./useSearch.ts";
 
 type Props = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
-    width: number;
+    width: number
+    bindToAddressBar?: boolean
 }
 
-const Search = ({width, placeholder = 'What do you want to play?', ...rest}: Props) => {
-    const {onChange,onFocus,value} = useSearch();
+const Search = ({width, ...rest}: Props) => {
     return (
         <div className={styles.search} style={{width: `${width * 16}px`}}>
             <IoIosSearch id="search-icon" size={30}/>
             <input
                 type="text"
-                onFocus={onFocus}
-                value={value}
-                onChange={onChange}
-                placeholder={placeholder}
                 {...rest}
             />
         </div>
