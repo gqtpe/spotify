fe9005d3a198dd1540900e70e5888e4dd077e66e
gqtpe <work.temirtas@gmail.com>
2024-09-10 13:43:30 +0500

    added components to centralized import

--- src/features/Browse/index.ts
+++ src/features/Browse/index.ts
@@ -1,6 +1,9 @@
+import { Browse } from "./Browse.tsx";
 import slice, {asyncActions} from "./browseSlice.ts";
 import Search from "./Search/Search.tsx";
 import {useSearch} from "./Search/useSearch/useSearch.ts";
+import Playlists from "./SearchPages/Playlists/Playlists.tsx";
+import Tracks from "./SearchPages/Tracks/Tracks.tsx";
 
 const browseReducer = slice.reducer
 const browseActions = {
@@ -13,5 +16,8 @@ export {
     browseReducer,
     Search,
     browseActions,
-    useSearch
+    useSearch,
+    Tracks,
+    Browse,
+    Playlists
 }
