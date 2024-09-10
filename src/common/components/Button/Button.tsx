2cc8500941353dfc90ffaf433a88903de496f279
gqtpe <work.temirtas@gmail.com>
2024-09-10 13:43:09 +0500

    added icon button variant

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -3,16 +3,16 @@ import styles from './Button.module.scss'
 import {Link} from "react-router-dom";
 
 
-type ButtonVariants = 'filled'|'outlined'|'text'
+type ButtonVariants = 'filled'|'outlined'|'text'|'icon'
 
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     variant?:ButtonVariants
     link?:string
 }
+//todo: normal icon variant
 const Button = ({children, variant = 'filled', className,link, ...rest}: Props) => {
-    const variantStyles = variant === 'filled'? styles.filled : variant === 'outlined'? styles.outlined : styles.text;
 
-    const resultClassName = `${styles.button} ${variantStyles} ${className}`;
+    const resultClassName = `${styles.button} ${styles[variant]} ${className}`;
 
     if(link){
         return <Link className={resultClassName + ' ' + styles.buttonLink} to={link}>
