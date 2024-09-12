9d35fb78d09c82353e3fee6f9202b9429c241edd
gqtpe <work.temirtas@gmail.com>
2024-09-12 10:34:32 +0500

    added browse selectors

---
+++ src/features/Browse/selectors.ts
@@ -0,0 +1,16 @@
+import {AppRootStateType} from "../Application/types.ts";
+
+
+export const selectPlaylists = (state: AppRootStateType) => state.browse.items.playlists
+
+export const selectTracks = (state: AppRootStateType) => state.browse.items.tracks
+
+export const selectAlbums = (state: AppRootStateType) => state.browse.items.albums
+
+export const selectArtists = (state: AppRootStateType) => state.browse.items.artists
+
+export const selectEpisodes = (state: AppRootStateType) => state.browse.items.episodes
+
+export const selectAudiobooks = (state: AppRootStateType) => state.browse.items.audiobooks
+
+export const selectShows = (state: AppRootStateType) => state.browse.items.shows
