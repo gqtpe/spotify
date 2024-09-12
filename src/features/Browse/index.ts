110c440f6fd751f7b17ff42581b0762a092aeede
gqtpe <work.temirtas@gmail.com>
2024-09-12 10:36:19 +0500

    added All page, selectors to centralized improts

--- src/features/Browse/index.ts
+++ src/features/Browse/index.ts
@@ -1,9 +1,11 @@
-import { Browse } from "./Browse.tsx";
+import {Browse} from "./Browse.tsx";
 import slice, {asyncActions} from "./browseSlice.ts";
 import Search from "./Search/Search.tsx";
 import {useSearch} from "./Search/useSearch/useSearch.ts";
-import Playlists from "./SearchPages/Playlists/Playlists.tsx";
 import Tracks from "./SearchPages/Tracks/Tracks.tsx";
+import * as browseSelectors from "./selectors.ts"
+import AllPage from "./SearchPages/All/All.tsx";
+
 
 const browseReducer = slice.reducer
 const browseActions = {
@@ -16,8 +18,9 @@ export {
     browseReducer,
     Search,
     browseActions,
+    browseSelectors,
     useSearch,
     Tracks,
     Browse,
-    Playlists
+    AllPage
 }
