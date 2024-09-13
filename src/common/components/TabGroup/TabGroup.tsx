53a0d4cc9a18dc15e0e2c3bba22b517b14745c34
gqtpe <work.temirtas@gmail.com>
2024-09-13 23:01:49 +0500

    made the component cleaner

--- src/common/components/TabGroup/TabGroup.tsx
+++ src/common/components/TabGroup/TabGroup.tsx
@@ -2,17 +2,14 @@ import {Children, cloneElement, isValidElement, memo, ReactNode} from "react";
 import styles from './TabGroup.module.scss'
 import TabItem from "./TabItem/TabItem.tsx";
 import {Tabs} from "../../../features/Browse/browseSlice.ts";
-import {useNavigate} from "react-router-dom";
 
 type Props = {
     children: ReactNode[]
-    value: string
+    value: string | null
     onChange: (value: Tabs) => void
 }
 
 const TabGroup = ({onChange, value, children}: Props) => {
-    const navigate = useNavigate()
-
     return <div className={styles.tabs}>
         <div className={styles.container}>
             {Children.map(children, (child) =>
@@ -22,11 +19,6 @@ const TabGroup = ({onChange, value, children}: Props) => {
                         active: child.props.value === value,
                         onClick: () => {
                             onChange(child.props.value)
-                            if(child.props.value === 'all'){
-                                navigate('')
-                            }else{
-                                navigate(`${child.props.value}`)
-                            }
                         }
                     })
                     : null
