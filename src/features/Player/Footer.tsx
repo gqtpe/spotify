f191fc326f88fda10f819d0b950f6a9141d1ff55
gqtpe <work.temirtas@gmail.com>
2024-09-13 17:14:32 +0500

    Footer component section added

---
+++ src/features/Player/Footer.tsx
@@ -0,0 +1,9 @@
+import {FC} from "react";
+import styles from "./Footer.module.scss";
+
+
+export const Footer: FC = () =>{
+    return <footer className={styles.footer}>
+            footer
+    </footer>
+}
