899102c6b63197920186f45b0a67125466da44ec
gqtpe <work.temirtas@gmail.com>
2024-09-13 17:15:38 +0500

    added IconButton

---
+++ src/common/components/IconButton/IconButton.tsx
@@ -0,0 +1,17 @@
+import {DetailsHTMLAttributes, FC, memo} from "react"
+import styles from "./IconButton.module.scss";
+
+
+type IconButtonProps = DetailsHTMLAttributes<HTMLButtonElement> & {
+    variant?: 'filled' | 'outlined' | 'icon',
+    fz?: number
+}
+
+const IconButton: FC<IconButtonProps> = ({variant = 'filled', children, className, fz, ...rest}) => {
+    return <button className={[className, styles.button, styles[variant]].join(' ')} style={{fontSize: fz, ...rest.style}} {...rest}>
+        {children}
+    </button>
+}
+
+
+export default memo(IconButton)
