f3ec38f32e81757f93c5a97ec89aa2d22b15b022
gqtpe <work.temirtas@gmail.com>
2024-09-13 17:13:52 +0500

    todo done, moved iconButton to separate component

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -3,13 +3,12 @@ import styles from './Button.module.scss'
 import {Link} from "react-router-dom";
 
 
-type ButtonVariants = 'filled'|'outlined'|'text'|'icon'
+type ButtonVariants = 'filled'|'outlined'|'text'
 
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     variant?:ButtonVariants
     link?:string
 }
-//todo: normal icon variant
 const Button = ({children, variant = 'filled', className,link, ...rest}: Props) => {
 
     const resultClassName = `${styles.button} ${styles[variant]} ${className}`;
