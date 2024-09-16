84e0977ed3a403a6f48d1fb2bbe49a43ef7cfe58
gqtpe <work.temirtas@gmail.com>
2024-09-16 08:32:30 +0500

    player: added centralized imports

--- src/features/Player/index.ts
+++ src/features/Player/index.ts
@@ -1,9 +1,14 @@
 import { Footer } from "./Footer";
+import slice from "./playerSlice";
 
 
-
-
+const playerReducer = slice.reducer
+const playerActions = {
+    ...slice.actions
+}
 
 export {
-    Footer
+    Footer,
+    playerReducer,
+    playerActions
 }
