7d02fc3b97150601c0c2111a8ee10b383124b20a
gqtpe <work.temirtas@gmail.com>
2024-09-16 08:25:44 +0500

    Add Button Component Stories with Variants in Storybook

--- src/common/components/Button/Button.stories.tsx
+++ src/common/components/Button/Button.stories.tsx
@@ -1,2 +1,45 @@
+import Button from "./Button.tsx";
+import {Meta, StoryObj} from "@storybook/react";
 
+
+const meta: Meta<typeof Button> = {
+    title: 'common/Button',
+    component: Button,
+    parameters: {
+        backgrounds: {
+            values: [
+                // 👇 Default values
+                {name: 'Dark', value: '#333'},
+            ],
+            default: 'Dark'
+        }
+    }
+}
+
+
+export default meta;
+
+type Story = StoryObj<typeof Button>
+
+
+export const Primary: Story = {
+    args: {
+        children: 'Button',
+        variant: 'filled',
+    }
+}
+
+export const Outlined: Story = {
+    args: {
+        children: 'Button',
+        variant: 'outlined'
+    },
+}
+
+export const Text: Story = {
+    args: {
+        children: 'Button',
+        variant: 'text'
+    }
+}
 
