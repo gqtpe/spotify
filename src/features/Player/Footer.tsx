795f6c20c44f3387cbb2ef5c7e19ac9c635badcd
gqtpe <work.temirtas@gmail.com>
2024-09-16 08:32:44 +0500

    player: added footer component

--- src/features/Player/Footer.tsx
+++ src/features/Player/Footer.tsx
@@ -3,7 +3,10 @@ import styles from "./Footer.module.scss";
 
 
 export const Footer: FC = () =>{
+    //todo:
+    // 3.1: add player slice
+    // 3.?: realize player with rtk
     return <footer className={styles.footer}>
-            footer
+
     </footer>
 }
