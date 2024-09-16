45d711a268e968d797063cafc58aac644e711f62
gqtpe <work.temirtas@gmail.com>
2024-09-16 08:26:53 +0500

    player: empty slice added

---
+++ src/features/Player/playerSlice.ts
@@ -0,0 +1,13 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+const slice = createSlice({
+    name: 'player',
+    initialState: {
+        playing: false
+    },
+    reducers: {},
+})
+
+
+export default slice
