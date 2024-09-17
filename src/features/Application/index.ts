7a961304068a38df0d0121c73a9981e349c718a5
gqtpe <work.temirtas@gmail.com>
2024-09-17 10:53:28 +0500

    removed redundant

--- src/features/Application/index.ts
+++ src/features/Application/index.ts
@@ -1,18 +1,11 @@
-import App from "../../app/App.tsx";
-import { store } from "../../app/store.ts";
-import * as appHooks from "./hooks"
-import {slice, asyncActions} from "./appSlice.ts";
+import {asyncActions, slice} from "./appSlice.ts";
 
 const appActions = {
     ...slice.actions,
     ...asyncActions
 }
-
 const appReducer = slice.reducer
 export {
-    App,
-    store,
-    appHooks,
     appReducer,
     appActions
 }
