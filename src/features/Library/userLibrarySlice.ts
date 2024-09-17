f89c4b09572209a81ef2cc2e2804815c8a033492
gqtpe <work.temirtas@gmail.com>
2024-09-17 10:59:18 +0500

    userLibrarySlice added, with user library fetching

---
+++ src/features/Library/userLibrarySlice.ts
@@ -0,0 +1,43 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {Album, SimplifiedPlaylist, spotifyAPI} from "../../api/spotifyAPI.ts";
+
+
+const fetchUserLibrary = createAsyncThunk<Array<SimplifiedPlaylist | Album>>('userLibrary/fetchUserLibrary', async (_, thunkAPI) => {
+    try {
+        const [r1, r2] = await Promise.all([
+            spotifyAPI.getSavedPlaylists(),
+            spotifyAPI.getSavedAlbums(),
+        ]);
+        const playlists = r1.data?.items || [];
+        const albums = r2.data?.items.map(t => t.album) || [];
+        return [...playlists, ...albums]
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e)
+    }
+})
+
+
+export const asyncActions = {
+    fetchUserLibrary
+}
+type Filter = 'all' | 'playlists' | 'album'
+const slice = createSlice({
+    name: 'userLibrary',
+    initialState: {
+        items: [] as SimplifiedPlaylist[] | Album[],
+        filter: 'all' as Filter
+    },
+    reducers: {
+        setFilter(state, action: { payload: Filter }) {
+            state.filter = action.payload
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchUserLibrary.fulfilled, (state, action) => {
+            state.items = action.payload
+        })
+    }
+})
+
+
+export default slice
