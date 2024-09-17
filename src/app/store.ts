30e7a4a4b5aa1969c10f07d617ae883051893acb
gqtpe <work.temirtas@gmail.com>
2024-09-17 10:57:27 +0500

    added library, player slices

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,18 +2,19 @@ import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {authReducer} from "../features/Auth";
 import {appReducer} from "../features/Application";
 import {browseReducer} from "../features/Browse";
-
+import {userLibraryReducer} from "../features/Library";
+import {playerReducer} from "../features/Player";
 
 export const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer,
     browse: browseReducer,
+    library: userLibraryReducer,
+    player: playerReducer,
 })
-
 export const store = configureStore({
     reducer:rootReducer,
     devTools: true,
 })
-
 
 
