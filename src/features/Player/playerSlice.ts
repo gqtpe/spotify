c98ddc9cb18ec43cc158778d2dbdb82d4a4c2f99
gqtpe <work.temirtas@gmail.com>
2024-09-18 11:42:12 +0500

    added fetchPlaybackState thunk, and fulfilled case

--- src/features/Player/playerSlice.ts
+++ src/features/Player/playerSlice.ts
@@ -1,12 +1,34 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {spotifyAPI, Track} from "../../api/spotifyAPI.ts";
+
+
+const fetchPlaybackState = createAsyncThunk<Track, undefined>('player/fetchPlaybackState', async (_, thunkAPI) => {
+    try {
+        const res = await spotifyAPI.getPlaybackState();
+        return res.data.item
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e)
+    }
+})
+
+
+export const authActions = {
+    fetchPlaybackState
+}
 
 
 const slice = createSlice({
     name: 'player',
     initialState: {
-        playing: false
+        item: null as null | Track,
+        queue: [] as Array<Track>,
     },
     reducers: {},
+    extraReducers: builder => {
+        builder.addCase(fetchPlaybackState.fulfilled, (state, action) => {
+            state.item = action.payload
+        })
+    }
 })
 
 
