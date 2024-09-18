ee90ab450c4ede91d63d8549a2ebfb7feb12184f
gqtpe <work.temirtas@gmail.com>
2024-09-18 11:45:46 +0500

    displayed current playing track

--- src/features/Player/Footer.tsx
+++ src/features/Player/Footer.tsx
@@ -1,12 +1,22 @@
 import {FC} from "react";
 import styles from "./Footer.module.scss";
+import {useAppSelector} from "../Application/hooks";
+import {AppRootStateType} from "../Application/types.ts";
+import {SimpleCard} from "../../common/components/SimpleCard/SimpleCard.tsx";
 
-
-export const Footer: FC = () =>{
+const selectPlayerItem = (state: AppRootStateType) => state.player.item
+export const Footer: FC = () => {
     //todo:
     // 3.1: add player slice
     // 3.?: realize player with rtk
-    return <footer className={styles.footer}>
+    // 3.2: auto update when track is changing outside?
+    const item = useAppSelector(selectPlayerItem)
 
-    </footer>
+    if (item) {
+        const subTitle = item.album.artists.map(artist => artist.name).join(', ')
+        return <footer className={styles.footer}><SimpleCard img={item.album.images[0]!.url} subtitle={subTitle}
+                                                             title={item.name}/></footer>
+    }
+
+    return
 }
