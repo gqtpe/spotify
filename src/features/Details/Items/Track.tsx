a05fa86d31a02a9da702af42848f0ecde375893a
gqtpe <work.temirtas@gmail.com>
2024-10-02 19:43:51 +0500

    detailed Track page

---
+++ src/features/Details/Items/Track.tsx
@@ -0,0 +1,15 @@
+import {FC} from "react";
+import type {Track} from "../../../api/spotifyAPI.ts";
+
+const Track: FC<{ item: Track }> = ({item}) => {
+    return (
+        <div>
+            <h1>{item.name}</h1>
+            <p>owner: {item?.artists.map(t => t.name).join(', ')}</p>
+            <p>{item.duration_ms}</p>
+            <img src={item.album.images[0].url} alt=""/>
+        </div>
+    );
+};
+
+export default Track;
