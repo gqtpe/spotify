f04a3b230f93b409345dd642cc596bed4acc4293
gqtpe <work.temirtas@gmail.com>
2024-10-02 19:43:43 +0500

    detailed Album page

---
+++ src/features/Details/Items/Album.tsx
@@ -0,0 +1,24 @@
+import {FC} from "react";
+import {type Album} from "../../../api/spotifyAPI.ts";
+
+
+const Album: FC<{ item: Album }> = ({item}) => {
+    return (
+        <div>
+            <h1>{item.name}</h1>
+            <p>owner: {item?.artists.map(t => t.name).join(', ')}</p>
+            <p>{item.release_date}</p>
+
+            <img src={item.images[0]?.url} alt={item?.name}/>
+            <ul>
+                {item.tracks.items.map((track) => (
+                    <li key={track.id}>
+                        {track.name} by {track.type === 'track' && track.artists[0].name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Album;
