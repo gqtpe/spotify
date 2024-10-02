e517312bc6df92d13f2346cad10f68fbc5ecc118
gqtpe <work.temirtas@gmail.com>
2024-10-02 19:43:19 +0500

    added navigation to detailed item page

--- src/common/components/Cards/Cards.tsx
+++ src/common/components/Cards/Cards.tsx
@@ -1,46 +1,47 @@
 import {FC} from "react";
 import {useAppSelector} from "../../../features/Application/hooks";
 import styles from '../../../features/Browse/Browse.module.scss'
-import type {Artist, Playlist, ResponseType, SimplifiedAlbum} from "../../../api/spotifyAPI.ts";
+import type {Artist, ResponseType, SimplifiedAlbum, SimplifiedPlaylist} from "../../../api/spotifyAPI.ts";
 import {AppRootStateType} from "../../../features/Application/types.ts";
 import Card from "./Card/Card.tsx";
+import {useNavigate} from "react-router-dom";
 
 type CardsProps = {
-    selector: (state: AppRootStateType) => ResponseType<Playlist[] | Artist[] | SimplifiedAlbum[]> | undefined
+    selector: (state: AppRootStateType) => ResponseType<SimplifiedPlaylist[] | Artist[] | SimplifiedAlbum[]> | undefined
     preview?: boolean
 };
 const Cards: FC<CardsProps> = ({selector, preview}) => {
     const item = useAppSelector(selector)
     //todo: process a case when the element does not have an image(throws error)
-    let resultElements;
+    const navigate = useNavigate()
     if (!item) {
         return <div>loading</div>
     }
-    debugger;
-    if(item){
-        resultElements = item.items.map(item => {
-            console.log(item)
-            let subTitle = ''
-            switch (item.type) {
-                case 'playlist':
-                    subTitle = 'By ' + item.owner.display_name;
-                    break;
-                case "album":
-                    subTitle = item.release_date.slice(0, 4) + ' - ' + item.artists.map(artist => artist.name).join(', ')
-                    break;
-                case "artist":
-                    subTitle = 'artist'
-                    break;
-            }
-            return <Card key={item.id} title={item.name} image={item.images[0].url} round={item.type === 'artist'}
-                         subtitle={subTitle}/>
-        })
-    }else{
-        resultElements = 'No results'
+    if (item.items.length === 0) {
+        return <div>no result founded</div>
+    }
+    const handleClick = (type: string, id: string) => {
+        navigate(`/${type}/${id}`)
     }
     return (
         <div className={styles.container + ' ' + (preview && styles.preview)}>
-            {resultElements}
+            {item.items.map(item => {
+
+                let subTitle = ''
+                switch (item.type) {
+                    case 'playlist':
+                        subTitle = 'By ' + item.owner.display_name;
+                        break;
+                    case "album":
+                        subTitle = item.release_date.slice(0, 4) + ' - ' + item.artists.map(artist => artist.name).join(', ')
+                        break;
+                    case "artist":
+                        subTitle = 'artist'
+                        break;
+                }
+                return <Card key={item.id} title={item.name} image={item.images[0].url} round={item.type === 'artist'}
+                             subtitle={subTitle} onClick={() => handleClick(item.type, item.id)}/>
+            })}
         </div>
     );
 };
