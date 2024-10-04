39d8be1a2a5a58dd2e383ebabb1e744e928f1ab2
gqtpe <work.temirtas@gmail.com>
2024-10-04 23:36:52 +0500

    Moved Track block to another component

--- src/features/Browse/SearchPages/All/All.tsx
+++ src/features/Browse/SearchPages/All/All.tsx
@@ -1,21 +1,14 @@
 import {FC, memo} from "react";
 import Typography from "../../../../common/components/Typography/Typography.tsx";
-import {browseSelectors, Tracks} from "../../";
+import {browseSelectors} from "../../";
 import Cards from "../../../../common/components/Cards/Cards.tsx";
 import styles from '../../Browse.module.scss'
+import {Songs} from "./Songs.tsx";
 
 
 const AllPage: FC = () => {
     return <div className={styles.all}>
-        <div className={styles.all__songs}>
-            <div>
-                <Typography variant="h4" className={styles.title}>Top result</Typography>
-            </div>
-            <div className={styles.songs}>
-                <Typography variant="h4" className={styles.title}>Songs</Typography>
-                <Tracks/>
-            </div>
-        </div>
+        <Songs/>
         <div className={styles.all__playlists}>
             <Typography variant="h4" className={styles.title}>Playlists</Typography>
             <Cards selector={browseSelectors.selectPlaylists} preview={true}/>
