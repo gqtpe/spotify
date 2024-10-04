908cdbcc0af983c050070737b606746f866f959c
gqtpe <work.temirtas@gmail.com>
2024-10-04 23:27:07 +0500

    trackColumn moved to separate file

---
+++ src/features/Browse/SearchPages/Tracks/trackColumns.tsx
@@ -0,0 +1,53 @@
+import {ColumnDef} from "@tanstack/react-table";
+import {Track} from "../../../../api/spotifyAPI.ts";
+import Typography from "../../../../common/components/Typography/Typography.tsx";
+import {MdExplicit} from "react-icons/md";
+import {FaRegClock} from "react-icons/fa";
+
+export const trackColumns: ColumnDef<Track>[] = [
+    {
+        header: () => <span>#</span>,
+        id: 'rowNumber',
+        cell: (info) => info.row.index + 1,
+    },
+    {
+        accessorKey: 'name',
+        header: 'Title',
+        cell: ({row}) => (
+            <div style={{display: 'flex', alignItems: 'center', gap: '8px'}}>
+                <img
+                    height={40}
+                    src={row.original.album.images[2].url}
+                    style={{borderRadius: '5px'}}
+                />
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <Typography>{row.original.name}</Typography>
+                    <Typography variant={'caption'} sx={{
+                        display: 'flex',
+                        gap: '4px',
+                        alignItems: 'center'
+                    }}>{row.original.explicit &&
+                        <MdExplicit fontSize={20}/>} {row.original.artists[0].name}</Typography>
+                </div>
+            </div>
+        ),
+    },
+    {
+        accessorKey: 'album.name',
+        header: 'Album',
+    },
+    {
+        cell: (track) => {
+            const duration = track.row.original.duration_ms;
+            const minute = Math.floor(duration / 60000);
+            let second = Math.floor((duration / 1000) % 60) + '';
+            if (second.length === 1) {
+                second = '0' + second;
+            }
+            return <Typography variant={'caption'}>{minute + ':' + second}</Typography>;
+        },
+        header: () => <FaRegClock />,
+        id: 'duration',
+        enableSorting: false,
+    },
+];
