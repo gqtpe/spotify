7250bd80791010ae147ed2ca92352564766625ef
gqtpe <work.temirtas@gmail.com>
2024-10-04 23:24:55 +0500

    added stories for Table component

---
+++ src/common/components/Table/Table.stories.tsx
@@ -0,0 +1,105 @@
+import {Meta, StoryObj} from "@storybook/react";
+import Table from "./Table.tsx";
+
+
+const meta: Meta<typeof Table> = {
+    title: 'common/Table',
+    component: Table
+}
+export default meta
+
+type Story = StoryObj<typeof Table>
+
+//todo: adjust styles
+type Person = {
+    firstName: string
+    lastName: string
+    age: number
+    visits: number
+    status: string
+    progress: number
+}
+const defaultData: Person[] = [
+    {
+        firstName: 'tanner',
+        lastName: 'linsley',
+        age: 24,
+        visits: 100,
+        status: 'In Relationship',
+        progress: 50,
+    },
+    {
+        firstName: 'tandy',
+        lastName: 'miller',
+        age: 40,
+        visits: 40,
+        status: 'Single',
+        progress: 80,
+    },
+    {
+        firstName: 'joe',
+        lastName: 'dirte',
+        age: 45,
+        visits: 20,
+        status: 'Complicated',
+        progress: 10,
+    },
+    {
+        firstName: 'tanner',
+        lastName: 'linsley',
+        age: 24,
+        visits: 100,
+        status: 'In Relationship',
+        progress: 50,
+    },
+    {
+        firstName: 'tandy',
+        lastName: 'miller',
+        age: 40,
+        visits: 40,
+        status: 'Single',
+        progress: 80,
+    },
+    {
+        firstName: 'tandy',
+        lastName: 'miller',
+        age: 40,
+        visits: 40,
+        status: 'Single',
+        progress: 80,
+    },
+    {
+        firstName: 'tandy',
+        lastName: 'miller',
+        age: 40,
+        visits: 40,
+        status: 'Single',
+        progress: 80,
+    },
+]
+const columns = [
+    {
+        header: () => <span>#</span>,
+        id: 'rowNumber',
+        width: 30,
+        cell: (info) => info.row.index + 1,
+    },
+    {
+        accessorKey: 'firstName',
+        header: 'Title',
+    },
+    {
+        accessorKey: 'lastName',
+        header: 'Album',
+    },
+    {
+        accessorKey: 'age',
+        header: 'duration',
+    },
+]
+export const Example: Story = {
+    args: {
+        columns: columns,
+        data: defaultData
+    }
+}
