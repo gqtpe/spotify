ea28aed0f39991e347de5067c5e2d2b32e932b56
gqtpe <work.temirtas@gmail.com>
2024-10-07 23:22:35 +0500

    added browse categories types

---
+++ src/api/types/browseCategories.ts
@@ -0,0 +1,14 @@
+import type {Images} from "./common.ts";
+
+
+interface CategoryObject {
+    href: string
+    icons: Images
+    id: string
+    name:string
+}
+
+
+export type {
+    CategoryObject
+}
