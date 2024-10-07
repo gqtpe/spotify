06b4cc28a03ad9e0cce5a829283dd8d5fb8afd73
gqtpe <work.temirtas@gmail.com>
2024-10-08 00:20:32 +0500

    moving type: convert duplicate types to interfaces and extend BasePlaylist

---
+++ src/api/types/playlist.ts
@@ -0,0 +1,57 @@
+import {External_Urls, Images, ResponseType} from "./common.ts";
+import {Track} from "./track.ts";
+import {Episode, SimplifiedUser} from "../spotifyAPI.ts";
+
+interface BasePlaylist {
+    collaborative: boolean
+    description: string
+    external_urls: External_Urls
+    href: string
+    id: string
+    images: Images
+    name: string
+    owner: SimplifiedUser
+    primary_color: null
+    public: boolean
+    snapshot_id: string
+
+    type: 'playlist'
+    uri: string
+}
+
+
+interface SimplifiedPlaylist extends BasePlaylist{
+    tracks:{
+        href: string
+        total: number
+    }
+}
+//playlist
+interface Playlist extends BasePlaylist {
+    external_urls: External_Urls
+    followers: {
+        href: string
+        total: number
+    }
+    snapshot_id: string
+    tracks: ResponseType<PlaylistTrackObject[]>
+}
+export type PlaylistTrackObject = {
+    added_at: string
+    added_by: {
+        external_urls: External_Urls
+        href: string
+        id: string
+        type: 'user'
+        uri: string
+    }
+    is_local: boolean
+    primary_color: string
+    track: Track | Episode
+}
+
+
+export type {
+    SimplifiedPlaylist,
+    Playlist
+}
