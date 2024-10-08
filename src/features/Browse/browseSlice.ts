c6760bafaea8ca39b1c5e6aa4bced1766ab14661
gqtpe <work.temirtas@gmail.com>
2024-10-08 11:24:28 +0500

    added categoryTiles to browseSlice

--- src/features/Browse/browseSlice.ts
+++ src/features/Browse/browseSlice.ts
@@ -2,26 +2,38 @@ import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {SearchResult, spotifyAPI} from "../../api/spotifyAPI.ts";
 import {AppRootStateType} from "../Application/types.ts";
 import {tabs} from "./tabs.ts";
+import {CategoryObject} from "../../api/types/browseCategories.ts";
+import {AxiosResponse} from "axios";
 
 export type Tabs = 'all' | 'track' | 'playlist' | 'album' | 'artist' | 'show' | 'episode' | 'audiobook'
 
-const browse = createAsyncThunk<any, { query: string, tab:Tabs }, { state: AppRootStateType }>('browse', async ({query, tab}, thunkAPI) => {
+const browse = createAsyncThunk<SearchResult, { query: string, tab:Tabs }, { state: AppRootStateType, rejectValue: unknown}>('browse', async ({query, tab}, thunkAPI) => {
     let activeTab:string = tab
     if(activeTab === 'all') {
         activeTab = tabs.slice(1).join('%2C')
     }
     try {
-        const response = await spotifyAPI.search(activeTab, query)
+        const response: AxiosResponse<SearchResult> = await spotifyAPI.search(activeTab, query)
         console.log(response)
         return response.data
     } catch (e) {
-        thunkAPI.rejectWithValue(e)
+        return thunkAPI.rejectWithValue(e)
+    }
+})
+const fetchBrowseCategories = createAsyncThunk<CategoryObject[], undefined, {rejectValue: unknown}>('browse/fetchBrowseCategories', async (_, thunkAPI) => {
+    try {
+        const response = await spotifyAPI.getBrowseCategories()
+        console.log(response)
+        return response.data.categories.items
+    }catch(e){
+        return thunkAPI.rejectWithValue(e)
     }
 })
 
 
 export const asyncActions = {
-    browse
+    browse,
+    fetchBrowseCategories
 }
 
 const slice = createSlice(
@@ -29,7 +41,8 @@ const slice = createSlice(
         name: 'browse',
         initialState: {
             activeTab: 'all' as Tabs,
-            items: {} as SearchResult
+            items: {} as SearchResult,
+            categories: [] as CategoryObject[],
         },
         reducers: {
             setActiveTab(state, action: PayloadAction<Tabs>) {
@@ -44,6 +57,9 @@ const slice = createSlice(
                 .addCase(browse.fulfilled, (state, action) => {
                     state.items = {...state.items, ...action.payload}
                 })
+            builder.addCase(fetchBrowseCategories.fulfilled, (state, action) => {
+                state.categories = action.payload
+            })
         }
     }
 )
