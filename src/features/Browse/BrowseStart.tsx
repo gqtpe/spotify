b60452862fd16dd1099144e7f2bee127c477206c
gqtpe <work.temirtas@gmail.com>
2024-10-08 11:25:41 +0500

    Added Browse category tiles to Browse Start

--- src/features/Browse/BrowseStart.tsx
+++
@@ -1,8 +0,0 @@
-import Typography from "../../common/components/Typography/Typography.tsx";
-import styles from "./Browse.module.scss"
-
-export const BrowseStart = () =>{
-    return <div className={styles.startPage}>
-        <Typography variant="h4" sx={{marginTop:32}}>Browse all</Typography>
-    </div>
-}
