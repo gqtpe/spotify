03b58cd593c044796834145289021c9f50f6da07
gqtpe <work.temirtas@gmail.com>
2024-10-09 11:27:51 +0500

    renamed BrowseTile ot GenreTile

---
+++ src/features/Browse/Genres/GenreTile/GenreTile.tsx
@@ -0,0 +1,44 @@
+import {DetailedHTMLProps, DetailsHTMLAttributes, FC, memo, useState,} from "react";
+import {CategoryObject} from "../../../../api/types/browseCategories.ts";
+import styles from "../../SearchPages/StartPage/BrowseStart.module.scss";
+import Typography from "../../../../common/components/Typography/Typography.tsx";
+import {useNavigate} from "react-router-dom";
+import {ImageColorThief} from "../../../../common/components/ImageColorThief/ImageColorThief.tsx";
+
+type Props = DetailedHTMLProps<
+    DetailsHTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+> & {
+    item: CategoryObject;
+};
+
+export const GenreTile: FC<Props> = memo(({ item }) => {
+    console.log("browse tile render");
+    const navigate = useNavigate();
+    const [color, setColor] = useState<string>("#ffffff");
+    const { icons, name, id } = item;
+
+    const handleClick = () => {
+        navigate(`/genre/${id}`);
+    };
+
+    return (
+        <div
+            className={styles.tile}
+            onClick={handleClick}
+            style={{ background: color }}
+        >
+            <Typography variant="h5">{name}</Typography>
+            <img
+                className={styles.tile__img}
+                src={icons[0].url}
+                alt={name}
+            />
+            <ImageColorThief
+                imageUrl={icons[0].url}
+                onColorDetected={setColor}
+            />
+        </div>
+    );
+});
+
