eccc76250204027e399f0826759a287bc49ea06a
gqtpe <work.temirtas@gmail.com>
2024-10-09 11:38:08 +0500

    GenrePage added

---
+++ src/features/Browse/Genres/Genre/GenrePage.tsx
@@ -0,0 +1,23 @@
+import {useFetchGenrePlaylists} from "./useFetchGenrePlaylists.ts";
+import Card from "../../../../common/components/Cards/Card/Card.tsx";
+import browseStyles from "../../Browse.module.scss";
+import styles from "./GenrePage.module.scss"
+import Typography from "../../../../common/components/Typography/Typography.tsx";
+
+const GenrePage = () => {
+    const {item} = useFetchGenrePlaylists()
+    if(!item){
+        return <div>Loading...</div>
+    }
+    return <div className={styles.page}>
+        <Typography variant="h3" className={styles.page__title}>{item.message}</Typography>
+        <div className={browseStyles.container}>
+        {
+            item.playlists!.items!.map(playlist => <Card cardID={playlist.id} key={playlist.id} type={playlist.type} variant={'default'} image={playlist.images[0]? playlist.images[0].url : ''} title={playlist.name} subtitle={'By ' + playlist.owner.display_name}/>)
+        }
+        </div>
+    </div>
+}
+
+
+export default GenrePage;
