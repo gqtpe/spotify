28a78807619e05b8ea0c130a61245cea53113896
gqtpe <work.temirtas@gmail.com>
2024-10-09 11:37:26 +0500

    Moved GenreTiles to Separate file

--- src/features/Browse/SearchPages/StartPage/BrowseStart.tsx
+++ src/features/Browse/SearchPages/StartPage/BrowseStart.tsx
@@ -1,18 +1,14 @@
 import Typography from "../../../../common/components/Typography/Typography.tsx";
 import styles from "./BrowseStart.module.scss"
-import {useBrowseCategories} from "./hooks/useBrowseCategories.ts";
-import {BrowseTile} from "./BrowseTile/BrowseTile.tsx";
+import Genres from "../../Genres/Genres.tsx";
 
 export const BrowseStart = () => {
-    const {categories} = useBrowseCategories()
     return <div className={styles.browseStart}>
         <div className={styles.browseStart__header}>
             <Typography variant="h4" sx={{marginTop: 32}}>Browse all</Typography>
         </div>
         <div className={styles.browseStart__tiles}>
-            {categories?.map(category => {
-                return <BrowseTile key={category.id} item={category}/>
-            })}
+            <Genres/>
         </div>
     </div>
 }
