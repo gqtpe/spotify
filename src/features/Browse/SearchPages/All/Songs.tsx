cec1623d665a9b22b5d8a7befc96deef0a90e5b3
gqtpe <work.temirtas@gmail.com>
2024-10-10 12:23:44 +0500

    Card: simpleCard replaced with card('default' property)

--- src/features/Browse/SearchPages/All/Songs.tsx
+++ src/features/Browse/SearchPages/All/Songs.tsx
@@ -2,9 +2,9 @@ import styles from "../../Browse.module.scss";
 import {useAppSelector} from "../../../Application/hooks";
 import {browseSelectors} from "../../index.ts";
 import Typography from "../../../../common/components/Typography/Typography.tsx";
-import {SimpleCard} from "../../../../common/components/Cards/SimpleCard/SimpleCard.tsx";
 import {trackColumns} from "../Tracks/trackColumns.tsx";
 import Table from "../../../../common/components/Table/Table.tsx";
+import Card from "../../../../common/components/Cards/Card/Card.tsx";
 
 
 export const Songs = () => {
@@ -19,7 +19,12 @@ export const Songs = () => {
         <div className={styles.all__songs}>
             <div>
                 <Typography variant="h4" className={styles.title}>Top result</Typography>
-                <SimpleCard img={topResult?.album?.images[0].url} title={topResult?.name} subtitle={topResult?.artists[0].name}/>
+                <Card
+                    image={topResult?.album?.images[0].url}
+                    title={topResult?.name}
+                    subtitle={topResult?.artists[0].name}
+                    explicit={topResult?.explicit}
+                />
             </div>
             <div className={styles.songs}>
                 <Typography variant="h4" className={styles.title}>Songs</Typography>
