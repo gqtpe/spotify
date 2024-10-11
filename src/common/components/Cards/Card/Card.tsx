3554126f1b2d95f8ac5d7ed0c4fb3bbfb2520604
gqtpe <work.temirtas@gmail.com>
2024-10-11 21:59:12 +0500

    stop propagation for Card

--- src/common/components/Cards/Card/Card.tsx
+++ src/common/components/Cards/Card/Card.tsx
@@ -1,15 +1,16 @@
-import {FC, memo, useCallback} from "react";
+import {FC, memo, MouseEvent, useCallback} from "react";
 import styles from './Card.module.scss'
 import Typography from "../../Typography/Typography.tsx";
 import {IoMdMusicalNote, IoMdPlay} from "react-icons/io";
 import IconButton from "../../IconButton/IconButton.tsx";
 import {MdExplicit} from "react-icons/md";
+import {useNavigate} from "react-router-dom";
 
 type PlaylistProps = {
     title: string
     image: string | null
     subtitle: string
-    variant?: 'default' | 'small'
+    variant?: 'default' | 'small' | 'large'
     round?: boolean
     onClick?: () => void
     onPlay?: () => void
@@ -17,7 +18,17 @@ type PlaylistProps = {
     link?: string
 }
 //todo: variants
-const Card: FC<PlaylistProps> = ({title, subtitle, image, round, onPlay,onClick, variant = 'default', explicit=false}) => {
+const Card: FC<PlaylistProps> = ({
+                                     title,
+                                     subtitle,
+                                     image,
+                                     round,
+                                     onPlay,
+                                     onClick,
+                                     variant = 'default',
+                                     explicit = false,
+                                     link
+                                 }) => {
     const cutTitle = title.length > 30 ? title.slice(0, 30) + '...' : title;
     const defaultImageStyles = [styles.image, round && styles.round].join(' ');
     const navigate = useNavigate()
@@ -43,24 +54,24 @@ const Card: FC<PlaylistProps> = ({title, subtitle, image, round, onPlay,onClick,
         <div className={defaultImageStyles}>
             <IoMdMusicalNote className={styles.emptyImageIcon}/>
         </div>
-    const iconButtonVariant = variant === 'small' ? 'icon' : 'filled';
-    const handleClick = useCallback(()=>{
-        if(onClick){
-            onClick()
-        }
-    },[onClick])
+
+    const classNames = [styles.card, styles[variant], (onPlay && styles.hover)].join(' ')
 
     return (
-        <div className={[styles.card, styles[variant], (onPlay && styles.hover)].join(' ')} onClick={handleClick}>
+        <div className={classNames} onClick={handleClick}>
             <div className={styles.card__image_wp}>
                 {imageElement}
-                {onPlay && <IconButton variant={iconButtonVariant} className={styles.popup} onClick={onPlay}>
-                    <IoMdPlay  style={{position: 'relative', left: '2px'}}/>
-                </IconButton>}
+                {onPlay &&
+                    <IconButton variant={iconButtonVariant} className={styles.popup} onClick={handlePlayButtonClick}>
+                        <IoMdPlay style={{position: 'relative', left: '2px'}}/>
+                    </IconButton>}
             </div>
             <div className={styles.card__details}>
                 <Typography className={styles.title}>{cutTitle}</Typography>
-                <Typography variant='caption' className={styles.subtitle}>{explicit ? <MdExplicit className={styles.explicitIcon} /> : ''}{subtitle}</Typography>
+                <Typography variant='caption' className={styles.subtitle}>
+                    {explicit ? <MdExplicit className={styles.explicitIcon}/> : ''}
+                    {subtitle}
+                </Typography>
             </div>
         </div>
     )
