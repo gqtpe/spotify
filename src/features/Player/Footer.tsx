e0e2b3d154b4e1ee5c8a526daa47f5381629dcef
gqtpe <work.temirtas@gmail.com>
2024-10-17 08:18:21 +0500

    refactoring

--- src/features/Player/Footer.tsx
+++ src/features/Player/Footer.tsx
@@ -1,36 +1,43 @@
 import {FC} from "react";
 import styles from "./Footer.module.scss";
 import {useAppSelector} from "../Application/hooks";
-import {AppRootStateType} from "../Application/types.ts";
 import Card from "../../common/components/Cards/Card/Card.tsx";
 import IconButton from "../../common/components/IconButton/IconButton.tsx";
-import {FaPause, FaRepeat, FaShuffle} from "react-icons/fa6";
+import {FaPause, FaPlay, FaRepeat, FaShuffle} from "react-icons/fa6";
 import {FaStepBackward, FaStepForward} from "react-icons/fa";
 import usePanelActions from "./hooks/usePanelActions.ts";
+import {playerSelectors} from "./index.ts";
 
-
-const selectPlayerItem = (state: AppRootStateType) => state.player.item
 export const Footer: FC = () => {
-
-    const {togglePlay, prev, next, repeat, shuffle} = usePanelActions()
+    const is_playing = useAppSelector(playerSelectors.selectIsPlaying)
+    const {togglePlay} = usePanelActions(is_playing)
     //todo:
     // 3.1: add player slice
     // 3.?: realize player with rtk
     // 3.2: auto update when track is changing outside?
-    const item = useAppSelector(selectPlayerItem)
-    if (!item) {
-        return
+
+
+    //TODO: need to bind with WEB PLAYBACK SDK
+    const playback = useAppSelector(playerSelectors.selectPlayback)
+    if(!playback.device) {
+        console.log('No playback')
+        return <footer className={styles.footer}>
+            <div className={styles.footer__actions}>
+                no device available
+            </div>
+        </footer>
     }
+
     let subTitle = 'null';
-    if (item.type === 'track') {
-        subTitle = item.album.artists.map(artist => artist.name).join(', ')
+    if (playback.item.type === 'track') {
+        subTitle = playback.item.album.artists.map(artist => artist.name).join(', ')
     }
     return <footer className={styles.footer}>
         <div className={[styles.footer__current, styles.current].join(' ')}>
-            <Card image={item.type === 'track' ? item.album.images[0]!.url : null}
+            <Card image={playback.item.type === 'track' ? playback.item.album.images[0]!.url : null}
                   subtitle={subTitle}
-                  title={item.name}
-                  explicit={item.explicit}
+                  title={playback.item.name}
+                  explicit={playback.item.explicit}
                   variant="small"
             />
         </div>
@@ -42,8 +49,8 @@ export const Footer: FC = () => {
                 <IconButton variant="icon">
                     <FaStepBackward/>
                 </IconButton>
-                <IconButton variant="outlined" className={styles.play}>
-                    <FaPause/>
+                <IconButton variant="outlined" className={styles.play} onClick={togglePlay}>
+                    {is_playing ? <FaPause/> : <FaPlay/>}
                 </IconButton>
                 <IconButton variant="icon">
                     <FaStepForward/>
@@ -55,6 +62,10 @@ export const Footer: FC = () => {
             <div className={styles.panel__slider}>
                 <input type="range"/>
             </div>
+        </div>
+        <div className={styles.panel__actions}>
+            deviceID:{playback.device.id}<br/>
+            deviceName:{playback.device.name}<br/>
         </div>
     </footer>
 }
