4b288f117bdbe1539eadc745b94da1e55ed39a96
gqtpe <work.temirtas@gmail.com>
2024-10-17 12:30:12 +0500

    spotify v1.0.0(removed client_ID client_SECRET from client side): refresh token checking

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -2,25 +2,32 @@ import Button from "../../common/components/Button/Button.tsx";
 import {spotifyAPI} from "../../api/spotifyAPI.ts";
 import {useAppSelector} from "../Application/hooks";
 import {playerSelectors} from "../Player";
+import {getItem} from "../../common/utils/localStorage.ts";
+import axios from "axios";
 
-const handleClick = async () =>{
+const handleClick = async () => {
     const response = await spotifyAPI.getMe()
-    console.log('me:',response.data)
+    console.log('me:', response.data)
 }
-const fetchAvailableDevices = async () =>{
+const fetchAvailableDevices = async () => {
     const response = await spotifyAPI.getAvailableDevices()
     console.log('devices:', response.data)
 }
-
+const refreshToken = async () => {
+    const refreshToken = getItem('refresh_token')
+    console.log(refreshToken)
+    const response = await axios.post('https://spotify-back-lovat.vercel.app/refresh', { refresh_token: refreshToken });
+    console.log('refresh Response:', response.data)
+}
 export const Home = () => {
     const playback = useAppSelector(playerSelectors.selectPlayback)
-    const showPlayback = ()=>{
+    const showPlayback = () => {
         console.log('playback:', playback)
     }
-
     return <div style={{padding: '16px'}}>
         <Button onClick={handleClick}>get me</Button>
         <Button onClick={fetchAvailableDevices}>get devices</Button>
         <Button onClick={showPlayback}>show Playback</Button>
+        <Button onClick={refreshToken}>refreshToken</Button>
     </div>
 }
