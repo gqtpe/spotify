4965ccfae02d39c08339b384af5fe042fc3b4a3c
gqtpe <work.temirtas@gmail.com>
2024-10-17 08:11:57 +0500

    added some helpers to home page

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,13 +1,26 @@
 import Button from "../../common/components/Button/Button.tsx";
 import {spotifyAPI} from "../../api/spotifyAPI.ts";
+import {useAppSelector} from "../Application/hooks";
+import {playerSelectors} from "../Player";
+
+const handleClick = async () =>{
+    const response = await spotifyAPI.getMe()
+    console.log('me:',response.data)
+}
+const fetchAvailableDevices = async () =>{
+    const response = await spotifyAPI.getAvailableDevices()
+    console.log('devices:', response.data)
+}
 
 export const Home = () => {
-
-    const handleClick = () =>{
-        const data = spotifyAPI.getMe().then(res=>res.data)
-        console.log(data)
+    const playback = useAppSelector(playerSelectors.selectPlayback)
+    const showPlayback = ()=>{
+        console.log('playback:', playback)
     }
+
     return <div style={{padding: '16px'}}>
         <Button onClick={handleClick}>get me</Button>
+        <Button onClick={fetchAvailableDevices}>get devices</Button>
+        <Button onClick={showPlayback}>show Playback</Button>
     </div>
 }
