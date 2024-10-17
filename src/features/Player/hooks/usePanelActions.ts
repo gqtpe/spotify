f98fbb394e76f644b4d693a9ccc07d6bea442c3e
gqtpe <work.temirtas@gmail.com>
2024-10-17 08:17:33 +0500

    pause action ready

--- src/features/Player/hooks/usePanelActions.ts
+++ src/features/Player/hooks/usePanelActions.ts
@@ -1,13 +1,32 @@
 import {useCallback} from "react";
+import {useActions} from "../../Application/hooks";
+import {playerActions} from "../index.ts";
 
 
-const usePanelActions = () =>{
-    const shuffle = useCallback(() =>  {}, [])
-    const prev = useCallback(() =>  {}, [])
-    const togglePlay = useCallback(() =>  {}, [])
-    const next = useCallback(() =>  {}, [])
-    const repeat = useCallback(() =>  {}, [])
-    return {togglePlay, shuffle, prev, next, repeat}
+const usePanelActions = (is_playing?: boolean) => {
+    const {pause, resume, setIsPlaying} = useActions(playerActions)
+
+    const shuffle = useCallback(() => {
+
+    }, [])
+    const prev = useCallback(() => {
+
+    }, [])
+    const togglePlay = useCallback(() => {
+        if (is_playing) {
+            pause()
+        } else {
+            resume()
+        }
+        setIsPlaying(!is_playing)
+    }, [is_playing])
+    const next = useCallback(() => {
+
+    }, [])
+    const repeat = useCallback(() => {
+
+    }, [])
+    return { togglePlay, shuffle, prev, next, repeat}
 }
 
 export default usePanelActions;
