52975e78dfc5671b70a87bc61f48bfe3931f3771
gqtpe <work.temirtas@gmail.com>
2024-10-17 12:29:57 +0500

    spotify v1.0.0(removed client_ID client_SECRET from client side): now you client will redirect to auth endpoint by their env

--- src/features/Auth/Login/Login.tsx
+++ src/features/Auth/Login/Login.tsx
@@ -1,17 +1,11 @@
 import Button from "../../../common/components/Button/Button.tsx";
-import {scopes} from "../../../api/scopes.ts";
 
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-const RESPONSE_TYPE = import.meta.env.VITE_RESPONSE_TYPE;
-const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
 const authEndpoint = import.meta.env.VITE_AUTH_ENDPOINT;
 
 const Login = () => {
-    console.log('Login')
-    const endpoint = `${authEndpoint}/?response_type=${RESPONSE_TYPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes}&show_dialog=true`
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '12px 0'}}>
-            <Button variant="filled" link={endpoint}>Log In</Button>
+            <Button variant="filled" link={authEndpoint}>Log In</Button>
         </div>
     );
 };
