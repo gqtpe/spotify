3b44847f09ae41023a952dee8dc7c896821f41f4
gqtpe <work.temirtas@gmail.com>
2024-10-31 13:36:08 +0500

    spotify v1.0.1(fixing redundant request when switching filters): removed unnecessary from useSearch custom hook

--- src/features/Browse/Search/useSearch/useSearch.ts
+++ src/features/Browse/Search/useSearch/useSearch.ts
@@ -1,71 +1,34 @@
 import {useActions, useAppSelector} from "../../../Application/hooks";
-import {browseActions} from "../../index.ts";
-import {useLocation, useNavigate} from "react-router-dom";
+import {browseActions, browseSelectors} from "../../index.ts";
 import {ChangeEvent, useCallback, useEffect, useState} from "react";
 
+export const useSearch = (navigate: (path: string) => void) => {
+    const query = useAppSelector(browseSelectors.selectQuery)
+    const activeTab = useAppSelector(browseSelectors.selectActiveTab)
+    const {setQuery} = useActions(browseActions)
+    //---
+    const [value, setValue] = useState(()=>query)
+    const [timeoutID, setTimeoutID] = useState<string | number | NodeJS.Timeout | undefined>( undefined)
+    //---
 
-export const useSearch = (navigate: ReturnType<typeof useNavigate>) =>{
-    const location = useLocation()
-
-    const [value, setValue] = useState('')
-    const [timeoutId, setTimeoutId] = useState<ReturnType<typeof setTimeout> | null>(null)
-    const activeTab = useAppSelector(state => state.browse.activeTab)
-    const {browse,clearItems} = useActions(browseActions)
-    //
     const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-         // all tab not shown in address bar
-        const query = e.target.value
-        if(!query){
-            navigate('/search/')
-        }
-
-        setValue(query)
-    }, [location.pathname, navigate])
-
-    const handleSearch = useCallback((query: string) => {
-        if(!query) return
-        browse({query, tab: activeTab})
-    }, [browse, activeTab])
-
-    const onFocus = useCallback(() => {
-        if (!location.pathname.includes('/search')) {
-            navigate('/search');
-        }
-    }, [location, navigate])
-
-
-    useEffect(() => {
-        if (location.pathname.includes('/search')) {
-            const query = location.pathname.split('/')[2]
-            if (query) {
-                setValue(query.replace('%20', ' '))
-            }
-
-        }
-    }, [])
+        setValue(e.target.value)
+    }, [setValue])
+    const handleSubmit = useCallback((value: string) => {
+        navigate(`/search/${value}${activeTab==='all'?'':'/' + activeTab}`)
+        setQuery(value)
+    },[activeTab, navigate, setQuery])
 
     useEffect(() => {
         if (value) {
-            if (timeoutId) {
-                clearTimeout(timeoutId)
-            }
-            const newTimeoutId = setTimeout(() => {
-                clearItems();
-                navigate(`/search/${value}${activeTab !== 'all' ? '/' + activeTab : ''}`);
-                handleSearch(value)
-                // browse({query: value, tab: activeTab})
-            }, 1000)
-            setTimeoutId(newTimeoutId)
-        }
-        return () => {
-            if (timeoutId) {
-                clearTimeout(timeoutId)
-            }
+            clearTimeout(timeoutID)
+            const currID = setTimeout(() => {
+                handleSubmit(value)
+            }, 500)
+            setTimeoutID(currID)
+            return () => clearTimeout(timeoutID)
         }
     }, [value])
-    useEffect(()=>{
-        handleSearch(value)
-    },[activeTab])
 
-    return {value, onChange, onFocus}
+    return {value, onChange}
 }
