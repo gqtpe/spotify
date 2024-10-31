5208d4ef22e1a0611e8411abbc2f5294bd1afa01
gqtpe <work.temirtas@gmail.com>
2024-10-31 13:40:40 +0500

    spotify v1.0.1: setting query and tab on use initialize

--- src/app/useInit.ts
+++ src/app/useInit.ts
@@ -1,13 +1,20 @@
 import {useEffect} from "react";
 import {useActions, useAppSelector} from "../features/Application/hooks";
 import {appActions} from "../features/Application";
+import {Tabs} from "../features/Browse/browseSlice.ts";
+import {useLocation} from "react-router-dom";
+import {browseActions} from "../features/Browse";
 
 
 export const useInit = () => {
-
     const isInitialized = useAppSelector(state => state.app.isInitialized)
     const user = useAppSelector(state => state.auth.user)
     const {initializeApp} = useActions(appActions)
+    //browse
+    const location = useLocation()
+    const {setActiveTab, setQuery} = useActions(browseActions)
+
+
     useEffect(() => {
         document.title = 'Spotify'
         setTimeout(() => {
@@ -15,7 +22,18 @@ export const useInit = () => {
         }, 500)
 
     }, [initializeApp])
-
+    useEffect(() => {
+        if (location.pathname.includes('/search')) {
+            if (location.pathname.split('/')[2]) {
+                setQuery(location.pathname.split('/')[2])
+                if (location.pathname.split('/')[3]) {
+                    setActiveTab(location.pathname.split('/')[3] as Tabs)
+                } else {
+                    setActiveTab('all')
+                }
+            }
+        }
+    }, [])
 
     return {isInitialized, user}
 }
