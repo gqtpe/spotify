60a32d3bc4e097695606fdc5482cd1dd94341abc
gqtpe <work.temirtas@gmail.com>
2024-11-04 12:02:10 +0500

    spotify v1.0.2(player panel actions): selectActiveDevice selector added

--- src/features/Player/selectors.ts
+++ src/features/Player/selectors.ts
@@ -1,8 +1,5 @@
 import {AppRootStateType} from "../Application/types.ts";
 
-export const selectIsPlaying = (state: AppRootStateType) => state.player.playback?.is_playing
-export const selectPlayerItem = (state: AppRootStateType) => state.player.playback?.item
-export const selectPlayback = (state:AppRootStateType) => state.player.playback
 export const selectIsPlaying = (state: AppRootStateType) => state.player.isPlaying
 
 export const selectPlaybackItem = (state: AppRootStateType) =>state.player.item
@@ -10,3 +7,5 @@ export const selectPlaybackItem = (state: AppRootStateType) =>state.player.item
 export const selectQueue = (state: AppRootStateType) => state.player.queue
 
 export const selectShuffleState = (state: AppRootStateType) => state.player.shuffleState
+
+export const selectActiveDevice = (state: AppRootStateType) => state.player.activeDevice
