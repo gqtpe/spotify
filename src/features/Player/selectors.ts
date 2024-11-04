fe73c6ccb4a9a4a76e37577fe9f4824631aa2574
gqtpe <work.temirtas@gmail.com>
2024-11-04 12:00:57 +0500

    spotify v1.0.2(player panel actions): selectIsPlayer selector added

--- src/features/Player/selectors.ts
+++ src/features/Player/selectors.ts
@@ -3,3 +3,4 @@ import {AppRootStateType} from "../Application/types.ts";
 export const selectIsPlaying = (state: AppRootStateType) => state.player.playback?.is_playing
 export const selectPlayerItem = (state: AppRootStateType) => state.player.playback?.item
 export const selectPlayback = (state:AppRootStateType) => state.player.playback
+export const selectIsPlaying = (state: AppRootStateType) => state.player.isPlaying
