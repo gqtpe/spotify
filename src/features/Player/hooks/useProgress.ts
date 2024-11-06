0b427029add5ff8868d82f893711e48bb089ce3d
gqtpe <work.temirtas@gmail.com>
2024-11-07 00:18:50 +0500

    spotify v1.0.3(Progress component): useProgress custom hook created

---
+++ src/features/Player/hooks/useProgress.ts
@@ -0,0 +1,17 @@
+import {useActions, useAppSelector} from "../../Application/hooks";
+import {playerActions, playerSelectors} from "../index.ts";
+import {useCallback} from "react";
+import {ActiveDevice} from "../playerSlice.ts";
+
+
+const useProgress = (device: ActiveDevice | null) => {
+    const {seekPosition} = useActions(playerActions)
+    const progress = useAppSelector(playerSelectors.selectProgress)
+    const callback = useCallback((position_ms: number) => {
+        if (device) {
+            seekPosition({position_ms: position_ms, deviceID: device.id})
+        }
+    }, [device, seekPosition])
+    return {progress, seekPosition: callback}
+}
+export default useProgress
