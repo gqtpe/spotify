ab86195481ad9ffdeaf7e5a29b3b9e0dd0bf87ae
gqtpe <work.temirtas@gmail.com>
2024-11-07 00:15:58 +0500

    spotify v1.0.3(seek progress): adjusted centralized imports

--- src/features/Player/index.ts
+++ src/features/Player/index.ts
@@ -1,7 +1,15 @@
 import { Footer } from "./Footer";
 import slice, {asyncAction} from "./playerSlice";
 import type {PlayerBackState} from "./types.ts";
-import * as playerSelectors from "./selectors.ts"
+import * as playerSelectors from "./selectors.ts";
+import * as timeHelpers from "./utils/helpers.ts";
+import useProgress from "./hooks/useProgress.ts";
+import usePlayerActions from "./hooks/usePlayerActions.ts";
+
+import Repeat from "./Repeat/Repeat.tsx";
+import ProgressBar from "./ProgressBar/ProgressBar.tsx";
+import Player from "./Player/Player.tsx";
+import Panel from "./Panel/Panel.tsx";
 
 const playerReducer = slice.reducer
 const playerActions = {
@@ -11,9 +19,16 @@ const playerActions = {
 }
 
 export {
-    Footer,
     playerReducer,
     playerActions,
     playerSelectors,
-    PlayerBackState
+    PlayerBackState,
+    timeHelpers,
+    usePlayerActions,
+    useProgress,
+    Footer,
+    Repeat,
+    ProgressBar,
+    Player,
+    Panel
 }
