6f481bf9455652351882fc1a47be5d10e970ed68
gqtpe <work.temirtas@gmail.com>
2024-11-13 17:23:25 +0500

    spotify v1.0.4(player):playbackState fetching button added to temp helpers on home page

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -2,6 +2,8 @@ import Button from "../../common/components/Button/Button.tsx";
 import {spotifyAPI} from "../../api/spotifyAPI.ts";
 import {getItem} from "../../common/utils/localStorage.ts";
 import axios from "axios";
+import {useActions} from "../Application/hooks";
+import {playerActions} from "../Player";
 
 const handleClick = async () => {
     const response = await spotifyAPI.getMe()
@@ -17,11 +19,16 @@ const refreshToken = async () => {
     const response = await axios.post('https://spotify-back-lovat.vercel.app/refresh', {refresh_token: refreshToken});
     console.log('refresh Response:', response.data)
 }
-export const Home = () => {
 
+export const Home = () => {
+    const {fetchPlaybackState} = useActions(playerActions)
+    const fetch = async () => {
+        fetchPlaybackState()
+    }
     return <div style={{padding: '16px'}}>
         <Button onClick={handleClick}>get me</Button>
         <Button onClick={fetchAvailableDevices}>get devices</Button>
         <Button onClick={refreshToken}>refreshToken</Button>
+        <Button onClick={fetch}>playbackState</Button>
     </div>
 }
