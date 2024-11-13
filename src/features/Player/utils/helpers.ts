a66bf2c310f74fbd00e8bbcbd0810931ed9f2aea
gqtpe <work.temirtas@gmail.com>
2024-11-13 17:22:43 +0500

    spotify v1.0.4(player):configured helper

--- src/features/Player/utils/helpers.ts
+++ src/features/Player/utils/helpers.ts
@@ -2,6 +2,6 @@ export const msToTime = (ms: number) => {
     let secondsPlayed = Math.floor(ms / 1000);
     const minutesPlayed = Math.floor(secondsPlayed / 60);
     secondsPlayed = secondsPlayed % 60;
-    return {sec: secondsPlayed, min: minutesPlayed}
+    return `${minutesPlayed}:${formatWithLeadingZero(secondsPlayed)}`
 }
 export const formatWithLeadingZero = (sec: number) => sec.toString().length === 1 ? `0${sec}` : sec
