1ffcb68f6ba79adf83be5d659ade71074d05c774
gqtpe <work.temirtas@gmail.com>
2024-11-23 20:06:58 +0500

    Spotify v1.0.8(reusable modal): user panel moved to Header.tsx

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,13 +1,18 @@
 import Badge from "../Badge/Badge.tsx";
-import {Search} from "../../../features/Browse";
-import {useCallback} from "react";
+import {Search, useSearch} from "../../../features/Browse";
+import {useCallback, useRef, useState} from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import {GoHomeFill} from "react-icons/go";
-import {useSearch} from "../../../features/Browse";
+import {IoIosNotificationsOutline} from "react-icons/io";
+import {useAppSelector} from "../../../features/Application/hooks";
+import {createPortal} from "react-dom";
+import Modal from "../Modal/Modal.tsx";
+import Paper from "../Modal/Paper.tsx";
 
 export const Header = () => {
     const navigate = useNavigate()
     const location = useLocation()
+    const [open, setOpen] = useState<boolean>(false)
     const {value, onChange} = useSearch(navigate)
     const onFocus = useCallback(() => {
         if (!location.pathname.includes('/search')) {
@@ -18,16 +23,34 @@ export const Header = () => {
     const handleClick = useCallback(() => {
         navigate('/')
     }, [])
-
-    return <header className="header">
-        <Badge variant="filled" onClick={handleClick}><GoHomeFill/></Badge>
-        <Search
-            width={25}
-            placeholder={'What do you want to play?'}
-            onFocus={onFocus}
-            value={value ? value : undefined}
-            onChange={onChange}
-        />
-    </header>
+    const user = useAppSelector(state => state.auth.user)
+    const anchorEl = useRef<HTMLDivElement>(null)
+    return <>
+        <header className="header">
+            <Badge variant="filled" onClick={handleClick}><GoHomeFill/></Badge>
+            <Search
+                width={25}
+                placeholder={'What do you want to play?'}
+                onFocus={onFocus}
+                value={value ? value : undefined}
+                onChange={onChange}
+            />
+        </header>
+        <div className="user-panel">
+            <Badge variant="icon" onClick={() =>setOpen((prev: boolean) => !prev)} ref={anchorEl}><IoIosNotificationsOutline/></Badge>
+            {open && anchorEl.current && createPortal(<Modal
+                anchorEl={anchorEl.current}
+                placement="bottom-end"
+                margin={24}
+            >
+                <Paper>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, deserunt doloribus et libero magni
+                    nulla rem sed voluptas voluptatibus. Consequatur dolor excepturi laborum nam, nisi nobis nulla
+                    obcaecati temporibus totam.
+                </Paper>
+            </Modal>, document.getElementById('portal')!)}
+            <Badge variant="filled"><img src={user?.images[0].url} alt="user-avatar"/></Badge>
+        </div>
+    </>
 }
 
