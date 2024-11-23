9674531d8a791c02e6e37e95d4021bf24d453118
gqtpe <work.temirtas@gmail.com>
2024-11-23 20:09:22 +0500

    Spotify v1.0.8(reusable modal):add ref to IconButton.tsx to support setting anchor for modal positioning

--- src/common/components/IconButton/IconButton.tsx
+++ src/common/components/IconButton/IconButton.tsx
@@ -1,17 +1,25 @@
-import {DetailsHTMLAttributes, FC, memo} from "react"
+import { forwardRef, DetailsHTMLAttributes } from "react";
 import styles from "./IconButton.module.scss";
-
 
 type IconButtonProps = DetailsHTMLAttributes<HTMLButtonElement> & {
     variant?: 'filled' | 'outlined' | 'icon',
     fz?: number
-}
+};
+
 //todo:add disabled property
-const IconButton: FC<IconButtonProps> = ({variant = 'filled', children, className, fz, ...rest}) => {
-    return <button className={[className, styles.button, styles[variant]].join(' ')} style={{fontSize: fz, ...rest.style}} {...rest}>
-        {children}
-    </button>
-}
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+    ({ variant = 'filled', children, className, fz, ...rest }, ref) => {
+        return (
+            <button
+                ref={ref}
+                className={[className, styles.button, styles[variant]].join(' ')}
+                style={{ fontSize: fz, ...rest.style }}
+                {...rest}
+            >
+                {children}
+            </button>
+        );
+    }
+);
 
-
-export default memo(IconButton)
+export default IconButton;
