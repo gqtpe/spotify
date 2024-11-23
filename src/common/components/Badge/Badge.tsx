ada5bdf06f6751c81c3167dbc5d8403d1f858a16
gqtpe <work.temirtas@gmail.com>
2024-11-23 20:05:43 +0500

    Spotify v1.0.8(reusable modal): add ref support to set anchor for modal positioning

--- src/common/components/Badge/Badge.tsx
+++ src/common/components/Badge/Badge.tsx
@@ -1,15 +1,18 @@
-import {DetailsHTMLAttributes, memo, ReactNode} from "react";
-import './Badge.scss'
+import { forwardRef, memo, ReactNode, DetailsHTMLAttributes } from "react";
+import './Badge.scss';
 
 type Props = DetailsHTMLAttributes<HTMLDivElement> & {
-    children: ReactNode
-    variant: 'filled' | 'icon'
-}
-const Badge = ({variant, children, ...rest}:Props) =>{
-    const variantClassName = variant === 'filled' ? 'filled' : 'icon'
-    return <div className={`badge ${variantClassName}`} {...rest}>
-        {children}
-    </div>
-}
+    children: ReactNode;
+    variant: 'filled' | 'icon';
+};
+
+const Badge = forwardRef<HTMLDivElement, Props>(({ variant, children, ...rest }, ref) => {
+    const variantClassName = variant === 'filled' ? 'filled' : 'icon';
+    return (
+        <div ref={ref} className={`badge ${variantClassName}`} {...rest}>
+            {children}
+        </div>
+    );
+});
 
 export default memo(Badge);
