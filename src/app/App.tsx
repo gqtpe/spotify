0750f43cd4678735471864c8c291d112a8244f99
gqtpe <work.temirtas@gmail.com>
2024-11-23 20:03:06 +0500

    Spotify v1.0.8:moved user-panel inside header component(react.fragment)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,5 @@
 import './App.scss'
 import {Outlet} from "react-router-dom";
-import Badge from "../common/components/Badge/Badge.tsx";
-import {IoIosNotificationsOutline} from "react-icons/io";
 import {useInit} from './useInit.ts';
 import {Header} from "../common/components/Header/Header.tsx";
 import {Footer} from "../features/Player";
@@ -9,7 +7,7 @@ import {Library} from "../features/Library";
 
 function App() {
     console.log('APP')
-    const {isInitialized, user} = useInit()
+    const {isInitialized} = useInit()
     //todo:
     // 4.1: /playlist/:id page
     // 4.?: analogous pages for tracks, albums, artists
@@ -23,10 +21,7 @@ function App() {
     return (
         <div className="app">
             <Header/>
-            <div className="user-panel">
-                <Badge variant="icon"><IoIosNotificationsOutline/></Badge>
-                <Badge variant="filled"><img src={user?.images[0].url} alt="user-avatar"/></Badge>
-            </div>
+
             <nav>
                 <Library/>
             </nav>
