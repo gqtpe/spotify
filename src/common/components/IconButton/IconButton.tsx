48937e15c16e81537c0def8d2b6ff2e97485d65f
gqtpe <work.temirtas@gmail.com>
2024-11-24 14:31:00 +0500

    Spotify v1.0.9:Rename Card.module.scss to Card.scss for External Styling

--- src/common/components/IconButton/IconButton.tsx
+++ src/common/components/IconButton/IconButton.tsx
@@ -1,19 +1,18 @@
-import { forwardRef, DetailsHTMLAttributes } from "react";
+import {ButtonHTMLAttributes, forwardRef} from "react";
 import styles from "./IconButton.module.scss";
 
-type IconButtonProps = DetailsHTMLAttributes<HTMLButtonElement> & {
-    variant?: 'filled' | 'outlined' | 'icon',
-    fz?: number
+type IconButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    variant?: 'filled' | 'outlined' | 'icon';
+    fz?: number;
 };
-
 //todo:add disabled property
 const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
-    ({ variant = 'filled', children, className, fz, ...rest }, ref) => {
+    ({variant = 'filled', children, className, fz, ...rest}, ref) => {
         return (
             <button
                 ref={ref}
                 className={[className, styles.button, styles[variant]].join(' ')}
-                style={{ fontSize: fz, ...rest.style }}
+                style={{fontSize: fz, ...rest.style}}
                 {...rest}
             >
                 {children}
