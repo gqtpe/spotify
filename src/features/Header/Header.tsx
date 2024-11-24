979e087e312393ba1706aece7b131a808376ffe6
gqtpe <work.temirtas@gmail.com>
2024-11-24 14:32:15 +0500

    Spotify v1.0.9:moved to feature

---
+++ src/features/Header/Header.tsx
@@ -0,0 +1,35 @@
+import Badge from "../../common/components/Badge/Badge.tsx";
+import {Search, useSearch} from "../Browse";
+import {useCallback} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import {GoHomeFill} from "react-icons/go";
+
+export const Header = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const {value, onChange} = useSearch(navigate)
+    const onFocus = useCallback(() => {
+        if (!location.pathname.includes('/search')) {
+            console.log('navigate')
+            navigate('/search');
+        }
+    }, [navigate, location.pathname])
+    const handleClick = useCallback(() => {
+        navigate('/')
+    }, [])
+
+    return <>
+        <header className="header">
+            <Badge variant="filled" onClick={handleClick}><GoHomeFill/></Badge>
+            <Search
+                width={25}
+                placeholder={'What do you want to play?'}
+                onFocus={onFocus}
+                value={value ? value : undefined}
+                onChange={onChange}
+            />
+        </header>
+
+    </>
+}
+
