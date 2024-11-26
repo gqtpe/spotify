745b148118454bbd80e361ce30aaaf3f4be9bb09
gqtpe <work.temirtas@gmail.com>
2024-11-26 23:11:37 +0500

    Spotify v1.1.0(right-sidebar): useSidebar custom hook used

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,13 @@ import {useInit} from './useInit.ts';
 import {Header} from "../features/Header/Header.tsx";
 import {Footer} from "../features/Player";
 import {Library} from "../features/Library";
+import {useSidebar} from "../features/Application/hooks/useSidebar.ts";
+import Aside from "../features/Application/components/Aside/Aside.tsx";
 
 function App() {
     console.log('APP')
     const {isInitialized} = useInit()
+    const {open, sidebarContentType} = useSidebar()
     if (!isInitialized) {
         return <div className="loader"/>
     }
