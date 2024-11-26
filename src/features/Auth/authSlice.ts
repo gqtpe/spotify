3cd3c374d5fbb9ca4e57163e70e9c7d0f9526ca3
gqtpe <work.temirtas@gmail.com>
2024-11-26 23:03:18 +0500

    Spotify v1.1.0(refactoring):resolve circular dependency between appSlice and authSlice by extracting thunks

--- src/features/Auth/authSlice.ts
+++ src/features/Auth/authSlice.ts
@@ -1,6 +1,6 @@
 import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {spotifyAPI, SpotifyTokenResponse, User} from "../../api/spotifyAPI.ts";
-import {initializeApp} from "../Application/appSlice.ts";
+import {appActions} from "../Application";
 
 export type Token = string
 
@@ -29,7 +29,7 @@ export const slice = createSlice({
     },
     reducers: {},
     extraReducers: builder => {
-        builder.addCase(initializeApp.fulfilled, (state, action: PayloadAction<SpotifyTokenResponse | null>) => {
+        builder.addCase(appActions.initializeApp.fulfilled, (state, action: PayloadAction<SpotifyTokenResponse | null>) => {
             if (action.payload?.access_token) {
                 state.authToken = action.payload!.access_token
                 state.refreshToken = action.payload!.refresh_token
