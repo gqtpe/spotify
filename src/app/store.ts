619a819b2925e53bb4d8243d9877ef406dfdbfca
gqtpe <work.temirtas@gmail.com>
2024-11-28 13:12:08 +0500

    Spotify v1.1.0(user logout):clearing store for auth/logout case

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,24 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import {AnyAction, combineReducers, configureStore} from "@reduxjs/toolkit";
 import {authReducer} from "../features/Auth";
 import {appReducer} from "../features/Application";
 import {browseReducer} from "../features/Browse";
 import {userLibraryReducer} from "../features/Library";
 import {playerReducer} from "../features/Player";
 
-export const rootReducer = combineReducers({
+const combinedReducer =  combineReducers({
     app: appReducer,
     auth: authReducer,
     browse: browseReducer,
     library: userLibraryReducer,
     player: playerReducer,
 })
+//todo: create action auth/logout
+export const rootReducer = (state: any, action: AnyAction) =>{
+    if(action.type === 'auth/logout'){
+        state = {}
+    }
+    return combinedReducer(state, action)
+}
 export const store = configureStore({
     reducer:rootReducer,
     devTools: true,
