177b8c74b5279b9fe768ce05a44efcf94fa30df1
gqtpe <work.temirtas@gmail.com>
2024-11-28 13:10:10 +0500

    Spotify v1.1.0(user logout):moved auth Async actions to authThunks.ts

--- src/features/Auth/authSlice.ts
+++ src/features/Auth/authSlice.ts
@@ -4,19 +4,6 @@ import {appActions} from "../Application";
 
 export type Token = string
 
-export const getMe = createAsyncThunk<User, void>('auth/getMe', async (_, {rejectWithValue}) => {
-    try {
-        const response = await spotifyAPI.getMe()
-        console.log('me:', response.data)
-        return response.data
-    } catch (e) {
-        return rejectWithValue(e)
-    }
-})
-
-export const asyncActions = {
-    getMe
-}
 
 export const slice = createSlice({
     name: 'auth',
