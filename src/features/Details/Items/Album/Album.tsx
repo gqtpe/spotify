0bbea48ea2d2a7e1d46f71f325a460d62369d5e4
gqtpe <work.temirtas@gmail.com>
2024-11-29 08:11:25 +0500

    Spotify v1.1.0(detailed album page): detailed album page added

---
+++ src/features/Details/Items/Album/Album.tsx
@@ -0,0 +1,56 @@
+import {FC} from "react";
+import "./Album.scss";
+import type {Album} from "../../../../api/types/album.ts";
+import Typography from "../../../../common/components/Typography/Typography.tsx";
+import TracksTable from "../../../../common/components/Table/Table.tsx";
+import {columns} from "./columns.tsx";
+
+
+const Album: FC<{ item: Album }> = ({item}) => {
+    return (
+        <div className="album">
+            <div className="album__header detailed-page-header">
+                <div className="album__image detailed-page-image">
+                    <img src={item?.images[0].url} alt={item?.name}/>
+                </div>
+                <div className="album__description">
+                    <Typography variant='subtitle1'>Album</Typography>
+                    <Typography className="album__title" variant='h1'>{item.name}</Typography>
+                    <div className="album__details">
+                        <Typography variant='subtitle1'>
+                            {item.artists[0].name}
+                        </Typography>•
+                        <Typography variant='subtitle1' sx={{color: 'var(--text400)'}}>
+                            {item.release_date.slice(0, 4)}
+                        </Typography>
+                        •
+                        <Typography sx={{color: 'var(--text400)'}} variant='subtitle1'>
+                            {item.total_tracks} tracks
+                        </Typography>
+
+                    </div>
+                    {/*<div className="playlist__stats">*/}
+                    {/*    <Typography variant='subtitle1'*/}
+                    {/*                sx={{color: 'var(--text-100)'}}>{item?.owner.display_name}</Typography>•*/}
+                    {/*    <Typography variant='subtitle1'*/}
+                    {/*                sx={{color: 'var(--text-400)'}}>{item?.followers.total} saves</Typography>•*/}
+                    {/*    <Typography variant='subtitle1'*/}
+                    {/*                sx={{color: 'var(--text-400)'}}>{item?.tracks.total} tracks</Typography>*/}
+                    {/*</div>*/}
+                </div>
+            </div>
+            <div className="album__content">
+                <div className="album__actions detailed-actions">
+                    ...actions
+                </div>
+                <div className="album__table detailed-page-table">
+                    <TracksTable columns={columns} data={item?.tracks.items} enableRowNumbering/>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Album;
