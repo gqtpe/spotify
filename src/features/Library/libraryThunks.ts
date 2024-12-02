6b53870ddac751f43f5acbbd04b3cc8c084794af
gqtpe <work.temirtas@gmail.com>
2024-12-02 18:15:40 +0500

    Spotify v1.1.1(detailed playlist actions): Refactored fetchUserLibrary by moving it to libraryThunks.ts for better separation of slice-specific async thunks.                                            Implemented the following new async actions:                                            saveItem: Saves a track, album, or artist to the user's library.                                            removeItem: Removes a track, album, or artist from the user's library.                                            toggleSavePlaylist: Toggles the saved state of a playlist in the user's library.                                            Updated the asyncActions object to include the newly added thunks for improved modularity and reusability.

---
+++ src/features/Library/libraryThunks.ts
@@ -0,0 +1,66 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {spotifyAPI} from "../../api/spotifyAPI.ts";
+import {SimplifiedPlaylist} from "../../api/types/playlist.ts";
+import type {Album} from "../../api/types/album.ts";
+
+export const fetchUserLibrary = createAsyncThunk<Array<SimplifiedPlaylist | Album>>('userLibrary/fetchUserLibrary', async (_, thunkAPI) => {
+    try {
+        const [r1, r2] = await Promise.all([
+            spotifyAPI.getSavedPlaylists(),
+            spotifyAPI.getSavedAlbums(),
+        ]);
+        const playlists = r1.data.items || [];
+        const albums = r2.data.items.map(album => album.album) || [];
+        return [...playlists, ...albums]
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e)
+    }
+})
+const saveItem = createAsyncThunk<undefined, {
+    type: 'track' | 'album' | 'artist',
+    ids: string[]
+}>('userLibrary/saveItem', async (params, thunkAPI) => {
+    const {type, ids} = params
+    try {
+        const response = await spotifyAPI.saveItem(type, ids)
+        return response.data
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e)
+    }
+})
+
+const removeItem = createAsyncThunk<undefined, {
+    type: 'track' | 'album' | 'artist',
+    ids: string[]
+}>('userLibrary/saveItem', async (params, thunkAPI) => {
+    const {type, ids} = params
+    try {
+        const response = await spotifyAPI.removeItem(type, ids)
+        return response.data
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e)
+    }
+})
+
+const toggleSavePlaylist = createAsyncThunk<{ saved: boolean }, {
+    playlist_id: string
+}>('userLibrary/toggleSavePlaylist', async (params, thunkAPI) => {
+    try {
+        const isSaved = await spotifyAPI.isSavedPlaylist(params.playlist_id)
+        if (isSaved.data[0]) {
+            await spotifyAPI.removePlaylist(params.playlist_id)
+        } else {
+            await spotifyAPI.savePlaylist(params.playlist_id)
+        }
+        return {saved: !isSaved.data[0]}
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e)
+    }
+})
+
+export const asyncActions = {
+    fetchUserLibrary,
+    saveItem,
+    removeItem,
+    toggleSavePlaylist
+}
