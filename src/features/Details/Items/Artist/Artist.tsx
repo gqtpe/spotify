4fa26fc973d7e2f24c390e40ce065252ca0f6a88
gqtpe <work.temirtas@gmail.com>
2024-12-05 10:22:10 +0500

    Spotify v1.1.2(artist follow button): Commit Body:                                       Added functionality to follow/unfollow an artist by integrating the useSave hook.                                       Implemented a followState state to manage the artist's follow status.                                       Added a handleClick function to toggle the follow status and save it using the useSave hook.                                       Integrated a useEffect hook to fetch and set the initial follow status of the artist from the Spotify API.                                       Replaced the "Follow" button with conditional rendering based on the followState. Display a skeleton loader when the follow state is being fetched.                                       Cleaned up the UI to handle loading and follow/unfollow actions more intuitively.

--- src/features/Details/Items/Artist/Artist.tsx
+++ src/features/Details/Items/Artist/Artist.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useCallback, useEffect, useState} from "react";
 import './Artist.scss';
 import type {Artist} from "../../../../api/types/artist.ts";
 import Typography from "../../../../common/components/Typography/Typography.tsx";
@@ -8,11 +8,28 @@ import {usePlayAction} from "../../../Player";
 import useArtistDetails from "./useArtistDetails.ts";
 import Discography from "./components/Discography.tsx";
 import TopTracks from "./components/TopTracks.tsx";
-
+import useSave from "../../../Library/useSave/useSave.ts";
+import Button from "../../../../common/components/Button/Button.tsx";
+import {spotifyAPI} from "../../../../api/spotifyAPI.ts";
+import Skeleton from "../../../../common/components/Skeleton/Skeleton.tsx";
+//todo: move checkIsItemSaved inside useSave
 const Artist: FC<{ item: Artist }> = ({item}) => {
         const play = usePlayAction()
         const {loading, artistDetails} = useArtistDetails(item.id);
+        const save = useSave('artist')
+    const [followState, setFollowState] = useState<boolean|null>(null)
 
+    const handleClick = useCallback(async()=>{
+        const response = await save([item.id])
+        setFollowState(response)
+    },[])
+    useEffect(() => {
+            const fetchFollowState = async () => {
+                const response = await spotifyAPI.checkIsItemSaved('artist', [item.id])
+                setFollowState(response.data[0])
+            }
+            fetchFollowState()
+        }, []);
         return (
             <div className="detailed-page artist">
                 <div className=" detailed-page__header artist__header ">
@@ -29,6 +46,7 @@ const Artist: FC<{ item: Artist }> = ({item}) => {
                     <IconButton fz={24} onClick={() => play({type: 'artist', context_uri: item?.uri})}>
                         <FaPlay/>
                     </IconButton>
+                    {followState === null ? <Button variant="outlined"><Skeleton>Following</Skeleton></Button> :<Button variant="outlined" onClick={handleClick}>{followState?'Following':'Follow'}</Button>}
                 </div>
                 {loading === 'succeeded' && artistDetails ?
                     <div className="artist__content">
