d1654020cbce154ae799ab2944cf8b1fd5af36bc
gqtpe <work.temirtas@gmail.com>
2024-12-05 10:44:10 +0500

    Spotify v1.1.2:deploy fix

--- src/features/Browse/Genres/Genre/useFetchGenrePlaylists.ts
+++ src/features/Browse/Genres/Genre/useFetchGenrePlaylists.ts
@@ -1,28 +1,21 @@
-import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {spotifyAPI} from "../../../../api/spotifyAPI.ts";
-import type {SimplifiedPlaylist} from "../../../../api/types/playlist.ts";
-import type {ResponseType} from "../../../../api/types/common.ts";
-
-
 export const useFetchGenrePlaylists = () => {
-    const [item, setItem] = useState<{message: string, playlists: ResponseType<SimplifiedPlaylist[]>}>()
-    const param = useParams<{ id: string }>()
-
-    useEffect(() => {
-        if(param.id){
-            const fetchPlaylists = async () => {
-                try {
-                    const response = await spotifyAPI.getCategoryPlaylists(param.id!)
-                    setItem(response.data)
-                } catch (e) {
-                    console.log(e)
-                }
-            }
-            fetchPlaylists()
-        }
-
-    }, [param.id])
-
-    return {item}
+    // const [item, setItem] = useState<{message: string, playlists: ResponseType<SimplifiedPlaylist[]>}>()
+    // const param = useParams<{ id: string }>()
+    //
+    // useEffect(() => {
+    //     if(param.id){
+    //         const fetchPlaylists = async () => {
+    //             try {
+    //                 const response = await spotifyAPI.getCategoryPlaylists(param.id!)
+    //                 setItem(response.data)
+    //             } catch (e) {
+    //                 console.log(e)
+    //             }
+    //         }
+    //         fetchPlaylists()
+    //     }
+    //
+    // }, [param.id])
+    //
+    // return {item}
 }
