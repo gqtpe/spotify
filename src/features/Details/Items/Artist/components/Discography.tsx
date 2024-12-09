7bdcdd1c11648fbe4bb352c00471d6100d3e4392
gqtpe <work.temirtas@gmail.com>
2024-12-10 00:08:40 +0500

    Spotify v1.1.3(user detailed page): Refactored Discography component
                                        Added memoization: Wrapped the component with memo to prevent unnecessary re-renders.
                                        Improved props: Added a title prop for better section labeling.
                                        Integrated usePlayAction: Enabled play functionality for albums using the onPlay callback.
                                        Enhanced structure: Introduced a Typography component for displaying the title with consistent styling.
                                        Updated navigation logic: Adjusted the link to dynamically reflect the album's type.

--- src/features/Details/Items/Artist/components/Discography.tsx
+++ src/features/Details/Items/Artist/components/Discography.tsx
@@ -1,28 +1,33 @@
 import type {SimplifiedAlbum} from "../../../../../api/types/album.ts";
-import {FC} from "react";
+import {FC, memo} from "react";
 import Card from "../../../../../common/components/Cards/Card/Card.tsx";
-import {useNavigate} from "react-router-dom";
+import usePlayAction from "../../../../Player/hooks/usePlayAction.ts";
+import Typography from "../../../../../common/components/Typography/Typography.tsx";
 
 type Discography = {
     albums: SimplifiedAlbum[]
+    title: string
 }
 
-const Discography: FC<Discography> = ({albums}) => {
-    const navigate = useNavigate()
+const Discography: FC<Discography> = ({albums,title}) => {
+    const play = usePlayAction()
     return (
-        <div className="artist__albums">
-            {albums.map(album => {
-                return <Card
-                    key={album.id}
-                    title={album.name}
-                    subtitle={album.artists[0].name}
-                    image={album.images[0]?.url}
-                    link={`/album/${album.id}`}
-                    navigate={navigate}
-                />
-            })}
-        </div>
+        <>
+            <Typography className="title" variant="h3">{title}</Typography>
+            <div className="artist__albums">
+                {albums.map(album => {
+                    return <Card
+                        key={album.id}
+                        title={album.name}
+                        subtitle={album.artists[0].name}
+                        image={album.images[0]?.url}
+                        onPlay={() => play({type: 'album', context_uri: album.uri})}
+                        link={`/${album.type}/${album.id}`}
+                    />
+                })}
+            </div>
+        </>
     );
 };
 
-export default Discography;
+export default memo(Discography);
