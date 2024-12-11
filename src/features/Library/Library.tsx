6d98240c91aa1cd4a68da40bfcba2f4cb598862c
gqtpe <work.temirtas@gmail.com>
2024-12-11 15:58:04 +0500

    Spotify v1.1.3(library links):Added titleLink and subtitleLink properties to the Card component for improved navigation capabilities.

--- src/features/Library/Library.tsx
+++ src/features/Library/Library.tsx
@@ -41,6 +41,8 @@ export const Library = () => {
                 variant="small"
                 link={`/${type}/${id}`}
                 onPlay={() => play({type: item.type, context_uri: item.uri, offset: {position: 0}})}
+                titleLink={`/${type}/${id}`}
+                subtitleLink={getSubtitleLink({item})}
             />
         );
     })
