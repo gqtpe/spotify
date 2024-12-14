b6e76dbc2b981f29f7fa3a47fc87847d1e46e6ed
gqtpe <work.temirtas@gmail.com>
2024-12-14 21:56:06 +0500

    Spotify v1.1.4(hint for all): added login information for viewing the page without logging in personal user account

--- src/features/Auth/Login/Login.tsx
+++ src/features/Auth/Login/Login.tsx
@@ -8,6 +8,15 @@ const Login = () => {
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '12px 0'}}>
             <Button variant="filled" link={authorize}>Log In</Button>
+        <div className="login">
+            <div className="login__hint">
+                <div className="login__hint-login">
+                    <span>login</span>
+                </div>
+                <div className="login__hint-password">
+                    <span>password</span>
+                </div>
+            </div>
         </div>
     );
 };
