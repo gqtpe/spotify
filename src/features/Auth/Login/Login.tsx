91a50afb7fa77dbf801e0ed662b820a1ee5deed7
gqtpe <work.temirtas@gmail.com>
2024-12-14 21:56:30 +0500

    Spotify v1.1.4(hint for all):copy to clipboard feature added

--- src/features/Auth/Login/Login.tsx
+++ src/features/Auth/Login/Login.tsx
@@ -5,16 +5,29 @@ const authEndpoint = import.meta.env.VITE_AUTH_ENDPOINT;
 const Login = () => {
     const authorize = authEndpoint + '/authorize'
     console.log(authorize)
+    const handleClick = (str: string) =>{
+        if(isCopied){
+            setIsCopied(false)
+        }
+        navigator.clipboard.writeText(str)
+        setIsCopied(true)
+        setTimeout(()=>setIsCopied(false), 2000)
+    }
+    const style:CSSProperties = {
+        opacity: isCopied ? 1 : 0,
+        transition: 'opacity, 0.3s ease-in-out'
+    }
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '12px 0'}}>
-            <Button variant="filled" link={authorize}>Log In</Button>
         <div className="login">
+            <div className="login__button"><Button variant="filled" link={authorize}>Log In</Button></div>
             <div className="login__hint">
                 <div className="login__hint-login">
                     <span>login</span>
+                    <b onClick={()=>handleClick('gqtpe-spotify-access@mail.ru')}>gqtpe-spotify-access@mail.ru</b>
                 </div>
                 <div className="login__hint-password">
                     <span>password</span>
+                    <b onClick={()=>handleClick('access-free-gqtpe')}>access-free-gqtpe</b>
                 </div>
             </div>
         </div>
