2073fe8fdb8ad7a43708628977c1427932f100d1
gqtpe <work.temirtas@gmail.com>
2024-12-14 22:24:03 +0500

    Spotify v1.1.4(search fix):added query param to navigation link onFocus

--- src/features/Application/components/Header/Header.tsx
+++ src/features/Application/components/Header/Header.tsx
@@ -1,18 +1,20 @@
 import Badge from "../../../../common/components/Badge/Badge.tsx";
-import {Search, useSearch} from "../../../Browse";
+import {browseSelectors, Search, useSearch} from "../../../Browse";
 import {useCallback} from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import {GoHomeFill} from "react-icons/go";
 import UserPanel from "./UserPanel.tsx";
+import {useAppSelector} from "../../hooks";
 
 export const Header = () => {
     const navigate = useNavigate()
     const location = useLocation()
     const {value, onChange} = useSearch(navigate)
+    const query = useAppSelector(browseSelectors.selectQuery)
     const onFocus = useCallback(() => {
-        if (!location.pathname.includes('/search')) {
-            console.log('navigate')
-            navigate('/search');
+        //why !includes, for prevent endless /search navigate
+        if (!location.pathname.includes('/search/')) {
+            navigate(`/search${query?'/' + query : ''}`);
         }
     }, [navigate, location.pathname])
     const handleClick = useCallback(() => {
