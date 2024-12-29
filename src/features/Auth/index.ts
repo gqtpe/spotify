995e366b495fdb0caf9f7b94910cdbf0e5f63b44
gqtpe <work.temirtas@gmail.com>
2024-12-29 14:58:16 +0500

    Spotify v1.1.5:added selectors to centralized import

--- src/features/Auth/index.ts
+++ src/features/Auth/index.ts
@@ -1,6 +1,7 @@
 import Login from "./Login/Login";
 import {slice} from "./authSlice.ts";
 import { asyncActions } from "./authThunks.ts";
+import * as authSelectors from "./selectors.ts";
 
 const authActions = {
     ...slice.actions,
@@ -11,5 +12,6 @@ const authReducer = slice.reducer
 export {
     Login,
     authActions,
-    authReducer
+    authReducer,
+    authSelectors
 }
