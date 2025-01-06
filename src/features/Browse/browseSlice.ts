3e9bccac28ffbde471534e31c6b639ad9eda09c6
gqtpe <work.temirtas@gmail.com>
2025-01-06 08:07:38 +0500

    Spotify v1.1.6(polish): moved asyncThunks from browse slice to separate file

--- src/features/Browse/browseSlice.ts
+++ src/features/Browse/browseSlice.ts
@@ -1,59 +1,11 @@
-import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {SearchResult, spotifyAPI} from "../../api/spotifyAPI.ts";
-import {AppRootStateType} from "../Application/types.ts";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {SearchResult} from "../../api/spotifyAPI.ts";
 import {CategoryObject} from "../../api/types/browseCategories.ts";
-import {AxiosError, AxiosResponse} from "axios";
 import type {RequestStatuses} from "../../api/types/common.ts";
-import {handleError} from "../../common/utils/error-utils.ts";
+import {browse, fetchBrowseCategories, fetchNewPortion} from "./browseThunks.ts";
 
 export type Tabs = keyof SearchResult | 'all'
 
-const browse = createAsyncThunk<SearchResult, { query: string, tab: Tabs }, {
-    state: AppRootStateType,
-    rejectValue: unknown
-}>('browse', async ({query, tab}, thunkAPI) => {
-    try {
-        const response: AxiosResponse<SearchResult> = await spotifyAPI.search(tab, query)
-        console.log(response)
-        return response.data
-    } catch (e) {
-        return handleError(e as AxiosError,thunkAPI.rejectWithValue)
-    }
-})
-const fetchBrowseCategories = createAsyncThunk<CategoryObject[], undefined, {
-    rejectValue: unknown
-}>('browse/fetchBrowseCategories', async (_, thunkAPI) => {
-    try {
-        const response = await spotifyAPI.getBrowseCategories()
-        console.log(response)
-        return response.data.categories.items
-    } catch (e) {
-        return handleError(e as AxiosError,thunkAPI.rejectWithValue)
-    }
-})
-const fetchNewPortion = createAsyncThunk<SearchResult>('browse/fetchNewPortionTracks', async (_, thunkAPI) => {
-    const store = thunkAPI.getState() as AppRootStateType
-    const activeTab = store.browse.activeTab
-    if (activeTab === 'all') {
-        return thunkAPI.rejectWithValue('no active tab')
-    }
-    const link = store.browse.items[activeTab]!.next
-    if (link) {
-        try {
-            const response = await spotifyAPI.getPortionOfItems(link)
-            return response.data
-        } catch (e) {
-            return handleError(e as AxiosError,thunkAPI.rejectWithValue)
-        }
-    }
-})
-
-
-export const asyncActions = {
-    browse,
-    fetchBrowseCategories,
-    fetchNewPortion,
-}
 const slice = createSlice/**/(
     {
         name: 'browse',
