c12842bd5e6d779cc30c2d65d7d067d460518d48
gqtpe <work.temirtas@gmail.com>
2025-01-08 19:01:30 +0500

    Spotify v1.1.6(polish): aside added

--- src/features/Application/components/Aside/Aside.tsx
+++ src/features/Application/components/Aside/Aside.tsx
@@ -1,12 +1,29 @@
 import {SidebarContent} from "../../appSlice.ts";
+import IconButton from "../../../../common/components/IconButton/IconButton.tsx";
+import { IoMdClose } from "react-icons/io";
+import './Aside.scss';
+import {Typography} from "../../../../common/components/Typography/Typography.tsx";
+import {firstToUpper} from "../../../Browse/utils/firstToUpper.ts";
+import {ReactNode} from "react";
 
 type Props ={
     state: SidebarContent
+    close: () =>void
+    children: ReactNode
 }
-const Aside = ({state}: Props) => {
+const Aside = ({state, close,children}: Props) => {
+    const handleRemove = () =>{
+        close()
+    }
     return (
-        <aside>
-            {state}
+        <aside className="aside">
+            <div className="aside__header">
+                <Typography variant="h4">{firstToUpper(state)}</Typography>
+                <IconButton variant="icon" onClick={handleRemove}>
+                    <IoMdClose/>
+                </IconButton>
+            </div>
+            {children}
         </aside>
     );
 };
