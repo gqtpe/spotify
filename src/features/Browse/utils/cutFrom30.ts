ffc512ce6d312ca1a57403aefa87f35330827124
gqtpe <work.temirtas@gmail.com>
2025-01-13 23:39:19 +0500

    Spotify v1.1.6(sidebar):added optional sliceInt params

--- src/features/Browse/utils/cutFrom30.ts
+++ src/features/Browse/utils/cutFrom30.ts
@@ -1,3 +1,3 @@
-export const cutFrom30 = (text: string) => {
-    return text.length > 30 ? `${text.slice(0, 30)}...` : text;
+export const cutFrom30 = (text: string, slice = 30) => {
+    return text.length > slice ? `${text.slice(0, slice)}...` : text;
 };
