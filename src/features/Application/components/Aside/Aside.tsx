38b47c88854b784a8364e5715d1504c64603312f
gqtpe <work.temirtas@gmail.com>
2025-01-14 23:19:18 +0500

    Spotify v1.1.7: refactoring

--- src/features/Application/components/Aside/Aside.tsx
+++ src/features/Application/components/Aside/Aside.tsx
@@ -1,24 +1,24 @@
 import {SidebarContent} from "../../appSlice.ts";
 import IconButton from "../../../../common/components/IconButton/IconButton.tsx";
-import { IoMdClose } from "react-icons/io";
+import {IoMdClose} from "react-icons/io";
 import './Aside.scss';
 import {Typography} from "../../../../common/components/Typography/Typography.tsx";
 import {firstToUpper} from "../../../Browse/utils/firstToUpper.ts";
 import {ReactNode} from "react";
 
-type Props ={
+type Props = {
     state: SidebarContent
-    close: () =>void
+    close: () => void
     children: ReactNode
 }
-const Aside = ({state, close,children}: Props) => {
-    const handleRemove = () =>{
+const Aside = ({state, close, children}: Props) => {
+    const handleRemove = () => {
         close()
     }
     return (
         <aside className="aside">
             <div className="aside__header">
-                <Typography variant="h4">{firstToUpper(state)}</Typography>
+                <Typography variant="h5">{firstToUpper(state)}</Typography>
                 <IconButton variant="icon" onClick={handleRemove}>
                     <IoMdClose/>
                 </IconButton>
