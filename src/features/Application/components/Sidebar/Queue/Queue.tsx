ee12939c889f35d8b195254c2d5eb27cda8a5a6d
gqtpe <work.temirtas@gmail.com>
2025-01-16 20:48:09 +0500

    Spotify v1.1.8(queue):queue component done

--- src/features/Application/components/Sidebar/Queue/Queue.tsx
+++ src/features/Application/components/Sidebar/Queue/Queue.tsx
@@ -1,11 +1,37 @@
+import {useActions, useAppSelector} from "../../../hooks";
+import {CurrentlyPlaying, playerActions, playerSelectors} from "../../../../Player";
+import Card from "../../../../../common/components/Cards/Card/Card.tsx";
+import {useEffect} from "react";
+import {cutFrom30} from "../../../../Browse/utils/cutFrom30.ts";
+import "./Queue.scss";
+import Typography from "../../../../../common/components/Typography/Typography.tsx";
 
-
-
-
-
-const Queue = () =>{
-    return <div>
-        Queue
+const Queue = () => {
+    const {fetchQueue} = useActions(playerActions)
+    const queue = useAppSelector(playerSelectors.selectQueue)
+    useEffect(() => {
+        fetchQueue()
+    }, [])
+    return <div className="queue">
+        <Typography variant="h6" className="current__subtitle">Now Playing</Typography>
+        <CurrentlyPlaying/>
+        <Typography variant="h6" className="current__subtitle">Queue</Typography>
+        <div className="queue__list">
+            {queue.map((t => {
+                const subtitle = t.album.artists.map(t => t.name).join(', ')
+                return <><Card
+                    key={t.id}
+                    title={cutFrom30(t.name)}
+                    subtitle={subtitle}
+                    image={t.album.images[1].url}
+                    explicit={t.explicit}
+                    variant="small-"
+                    link={`/track/${t.id}`}
+                    subtitleLink={`/artist/${t.album.artists[0].id}`}
+                />
+                </>
+            }))}
+        </div>
     </div>
 }
 
