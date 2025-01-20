d86b8b3a90f3f14978b5a3f15ce3c5fa8e0e6048
gqtpe <work.temirtas@gmail.com>
2025-01-20 17:33:00 +0500

    Spotify v1.1.8(storybook):default meta created

---
+++ src/common/components/Typography/Typography.stories.tsx
@@ -0,0 +1,15 @@
+const meta: Meta = {
+    title: 'common/Typography',
+    component: Typography,
+    tags: ['autodocs'],
+    parameters: {
+        backgrounds: {
+            values: [
+                // 👇 Default values
+                {name: 'Dark', value: '#000000'},
+            ],
+            default: 'Dark'
+        }
+    }
+}
+export default meta;
