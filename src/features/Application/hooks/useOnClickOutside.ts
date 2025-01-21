810f6eb0cb4314771f783e2e3519900267bd044b
gqtpe <work.temirtas@gmail.com>
2025-01-21 21:24:21 +0500

    Spotify v1.1.8(menu): useOnClickOutside.ts: add listener to all document

--- src/features/Application/hooks/useOnClickOutside.ts
+++ src/features/Application/hooks/useOnClickOutside.ts
@@ -1,4 +1,6 @@
-
+import {RefObject, useEffect} from "react";
+const useOnClickOutside = (anchor: RefObject<HTMLElement>, callback: (e: Event) => void) => {
+    useEffect(() => {
         //listener function checks if it clicked inside anchor it return, else call callback function
         const listener = (e: Event) => {
             const el = anchor.current
@@ -7,3 +9,12 @@
             }
             callback(e)
         }
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        }
+    }, [anchor, callback])
+}
+export default useOnClickOutside
