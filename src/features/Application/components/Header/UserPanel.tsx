7109c50ee14d2fe12b07320559cc471b009f37cc
gqtpe <work.temirtas@gmail.com>
2025-01-23 23:08:32 +0500

    Spotify v1.1.8: OutsideClick hoc added

--- src/features/Application/components/Header/UserPanel.tsx
+++ src/features/Application/components/Header/UserPanel.tsx
@@ -8,36 +8,41 @@ import {useAppSelector} from "../../hooks";
 import Button from "../../../../common/components/Button/Button.tsx";
 import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
+import OutsideClick from "../../../../common/hoc/OutisideClick/OutsideClick.tsx";
 
 const UserPanel = () => {
     const [open, setOpen] = useState<boolean>(false)
     const user = useAppSelector(state => state.auth.user)
     const dispatch = useDispatch()
     const anchorEl = useRef<HTMLDivElement>(null)
-    const onClick = () =>{
-        setOpen(prev =>!prev)
+    const onClick = () => {
+        setOpen(prev => !prev)
     }
     const navigate = useNavigate()
-    const logout = useCallback(() =>{
+    const logout = useCallback(() => {
         setOpen(false)
         localStorage.clear()
         dispatch({type: 'auth/logout'})
         navigate('/login')
-    },[])
+    }, [])
     return (
         <div className="user-panel">
-            <Badge variant="icon"
-                   ><IoIosNotificationsOutline/></Badge>
+            <Badge variant="icon">
+                <IoIosNotificationsOutline/>
+            </Badge>
             {open && anchorEl.current && createPortal(<Modal
                 anchorEl={anchorEl.current}
                 placement="bottom-end"
                 margin={8}
             >
-                <Paper>
+                <OutsideClick callback={onClick}>
+                    <Paper>
                     <Button variant="outlined" onClick={logout}>Log Out</Button>
                 </Paper>
+                </OutsideClick>
             </Modal>, document.getElementById('portal')!)}
-            <Badge variant="filled" onClick={onClick} ref={anchorEl}>{user?.images[0]?.url && <img src={user.images[0].url}/>}</Badge>
+            <Badge variant="filled" onClick={onClick} ref={anchorEl}>{user?.images[0]?.url &&
+                <img src={user.images[0].url}/>}</Badge>
         </div>
     );
 };
