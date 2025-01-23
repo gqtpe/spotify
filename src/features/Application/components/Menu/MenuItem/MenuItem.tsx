6d9c327c066a336e2aeb54d734d078cfc4342372
gqtpe <work.temirtas@gmail.com>
2025-01-23 22:45:33 +0500

    Spotify v1.1.8(menu): menu Item done

--- src/features/Application/components/Menu/MenuItem/MenuItem.tsx
+++ src/features/Application/components/Menu/MenuItem/MenuItem.tsx
@@ -1,17 +1,16 @@
-import {FC, ReactElement} from "react";
-import styles from "../Menu.module.scss";
+import {DetailedHTMLProps, FC, HTMLAttributes, ReactElement} from "react";
 import {Typography} from "../../../../../common/components/Typography/Typography.tsx";
 
 
 type Props = {
     title: string
     icon?: ReactElement,
-
-}
-const MenuItem: FC<Props> = ({title, icon}) => {
+    callback?: ()=>void
+} & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+const MenuItem: FC<Props> = ({title, icon, ...rest}) => {
     return (
-        <div className={[styles.option, styles.menu__item].join(' ')}>
-            <Typography icon={icon?icon:undefined}>{title}</Typography>
+        <div {...rest}>
+            <Typography icon={icon ? icon : undefined}>{title}</Typography>
         </div>
     );
 };
