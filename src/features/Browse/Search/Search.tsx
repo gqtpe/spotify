626b2e8c2583c33e76f505758b3d9dec33b42267
gqtpe <work.temirtas@gmail.com>
2025-02-05 13:13:53 +0500

    Spotify v1.2(adaptive ui): removed width param from search component

--- src/features/Browse/Search/Search.tsx
+++ src/features/Browse/Search/Search.tsx
@@ -3,12 +3,11 @@ import styles from "./Search.module.scss";
 import { FiSearch } from "react-icons/fi";
 
 type Props = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
-    width: number
 }
 
-const Search = ({width, ...rest}: Props) => {
+const Search = ({ ...rest}: Props) => {
     return (
-        <div className={styles.search} style={{width: `${width * 16}px`}}>
+        <div className={styles.search}>
             <FiSearch id="search-icon" className={styles.icon}/>
             <input
                 type="text"
