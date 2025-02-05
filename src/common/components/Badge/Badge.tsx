5809d318f739937b9015aaca3709289bd5d1cc78
gqtpe <work.temirtas@gmail.com>
2025-02-05 13:18:07 +0500

    Spotify v1.2(adaptive ui): refactoring

--- src/common/components/Badge/Badge.tsx
+++ src/common/components/Badge/Badge.tsx
@@ -6,10 +6,10 @@ type Props = DetailsHTMLAttributes<HTMLDivElement> & {
     variant: 'filled' | 'icon';
 };
 
-const Badge = forwardRef<HTMLDivElement, Props>(({ variant, children, ...rest }, ref) => {
-    const variantClassName = variant === 'filled' ? 'filled' : 'icon';
+const Badge = forwardRef<HTMLDivElement, Props>(({ variant, children,className, ...rest }, ref) => {
+    const variantClassName = variant === 'filled' ? 'badge-filled' : 'badge-icon';
     return (
-        <div ref={ref} className={`badge ${variantClassName}`} {...rest}>
+        <div ref={ref} className={`badge ${variantClassName} ${className}`} {...rest}>
             {children}
         </div>
     );
