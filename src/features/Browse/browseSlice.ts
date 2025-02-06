10d1f1a7252de2d9b3179bbc6c1a3843aa6cd4c0
gqtpe <work.temirtas@gmail.com>
2025-02-06 23:16:09 +0500

    Spotify v1.2(tests): browseSlice state type added

--- src/features/Browse/browseSlice.ts
+++ src/features/Browse/browseSlice.ts
@@ -5,6 +5,13 @@ import type {RequestStatuses} from "../../api/types/common.ts";
 import {browse, fetchBrowseCategories, fetchNewPortion} from "./browseThunks.ts";
 
 export type Tabs = keyof SearchResult | 'all'
+export type BrowseStateType = {
+    activeTab:  Tabs,
+    items:  SearchResult,
+    query: string|null,
+    loading: RequestStatuses,
+    categories:  CategoryObject[],
+}
 
 const slice = createSlice/**/(
     {
