2e484672e8d3986dac8f139df0fd596e969e2393
gqtpe <work.temirtas@gmail.com>
2025-02-06 23:18:47 +0500

    Spotify v1.2(tests appslice): created add unit tests for synchronous and asynchronous actions
                                - Validate setAppError and setAppInitialized actions
                                - Test openSidebar and closeSidebar behavior
                                - Confirm initializeApp async action handles both fulfilled and rejected cases

---
+++ src/features/Application/tests/appSlice.test.ts
@@ -0,0 +1,79 @@
+import {AppSliceInitialStateType, SidebarContent} from "../appSlice.ts";
+import {appActions, appReducer} from "../index.ts";
+import {beforeEach, describe, expect, test} from 'vitest'
+import {SpotifyTokenResponse} from "../../../api/spotifyAPI.ts";
+
+const {
+    setAppError,
+    setAppInitialized,
+    openSidebar,
+    closeSidebar,
+    initializeApp
+} = appActions;
+let startState: AppSliceInitialStateType;
+//
+beforeEach(() => {
+    startState = {
+        error: null,
+        isInitialized: false,
+        sidebar: {
+            isOpen: false,
+            content: null,
+        }
+    }
+})
+describe("appSlice synchronous actions", () => {
+    describe('setAppError',()=> {
+        test('should set app error', () => {
+            const newValue: string | null = 'Network Error';
+            const endState = appReducer(startState, setAppError(newValue))
+            expect(endState.error).toBe(newValue);
+        })
+        test('setAppError should remove app error', () => {
+            const endState = appReducer(startState, setAppError(null))
+            expect(endState.error).toBe(null);
+        })
+    })
+
+    describe('setAppInitialized ', () => {
+        test('should set is Initialized', () => {
+            const newValue: boolean = true;
+            const endState = appReducer(startState, setAppInitialized(newValue))
+            expect(endState.isInitialized).toBe(newValue);
+        })
+    })
+    describe('openSidebar',() =>{
+        test('should set isOpen true, and set its content',()=>{
+            const newValue: SidebarContent = 'devices';
+            const endState = appReducer(startState, openSidebar({type: newValue}))
+            expect(endState.sidebar.content).toBe(newValue);
+            expect(endState.sidebar.isOpen).toBe(true);
+        })
+        test('should set isOpen false, and clear content',()=>{
+            const endState = appReducer(startState, closeSidebar())
+            expect(endState.sidebar.content).toBe(null);
+            expect(endState.sidebar.isOpen).toBe(false);
+        })
+    })
+})
+describe("appSlice async actions", () => {
+    describe("initializeApp", () => {
+        test("should set isInitialized to true when fulfilled", () => {
+            const payload = {
+                access_token: '##########access',
+                refresh_token: '##########refresh',
+                expires_in: 17777777777,
+                token_type: 'code',
+                scope: '3333333',
+            } as SpotifyTokenResponse
+            const endState = appReducer(startState, initializeApp.fulfilled(payload, 'request-id'))
+            expect(endState.isInitialized).toBe(true);
+        });
+
+        test("should set isInitialized to false when rejected", () => {
+            const error = null
+            const endState = appReducer(startState, initializeApp.rejected(error, 'request-id',))
+            expect(endState.isInitialized).toBe(false);
+        });
+    });
+});
