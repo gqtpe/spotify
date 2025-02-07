8760b6334f7c8a4ba708c5cc43dd89000b597e49
gqtpe <work.temirtas@gmail.com>
2025-02-07 22:25:39 +0500

    Spotify v1.2(tests browseSlice): Add unit tests for browseSlice actions and reducers

--- src/features/Browse/tests/browseSlice.test.ts
+++ src/features/Browse/tests/browseSlice.test.ts
@@ -4,7 +4,7 @@ import type {BrowseStateType, Tabs} from "../browseSlice.ts";
 import {RequestStatuses} from "../../../api/types/common.ts";
 import {SearchResult} from "../../../api/spotifyAPI.ts";
 
-const {clearItems, browse, setQuery, setActiveTab} = browseActions;
+const {clearItems, browse, setQuery,fetchNewPortion, setActiveTab} = browseActions;
 let startState: BrowseStateType;
 //
 beforeEach(() => {
@@ -47,13 +47,13 @@ describe("browseSlice synchronous actions", () => {
                         items: [
                             {
                                 external_ids: {},
-                                available_markets:[],
+                                available_markets: [],
                                 disc_number: 9,
                                 uri: 'usr',
                                 href: 'trackhref',
                                 type: 'track',
                                 restrictions: {
-                                    reason:"explicit",
+                                    reason: "explicit",
                                 },
                                 explicit: false,
                                 external_urls: {},
@@ -126,13 +126,11 @@ describe("browseSlice synchronous actions", () => {
                 }
             };
 
-            console.log(modifiedState);
-
             const endState = browseReducer(modifiedState, clearItems())
-        expect(Object.keys(endState.items).length).toBe(0)
+            expect(Object.keys(endState.items)).toHaveLength(0)
+        })
     })
-})
-    describe('setQuery',()=>{
+    describe('setQuery', () => {
         test('should set query string for query string', () => {
             const newValue = 'query'
             const endState = browseReducer(startState, setQuery(newValue))
@@ -142,7 +140,7 @@ describe("browseSlice synchronous actions", () => {
 })
 describe("browseSlice async actions", () => {
     describe("browse", () => {
-        const browsePayload:{ query: string, tab: Tabs } = {tab:'artists', query:'sabrina carpenter'}
+        const browsePayload: { query: string, tab: Tabs } = {tab: 'artists', query: 'sabrina carpenter'}
         const response: SearchResult = {
             tracks: {
                 href: 'href',
@@ -154,13 +152,13 @@ describe("browseSlice async actions", () => {
                 items: [
                     {
                         external_ids: {},
-                        available_markets:[],
+                        available_markets: [],
                         disc_number: 9,
                         uri: 'usr',
                         href: 'trackhref',
                         type: 'track',
                         restrictions: {
-                            reason:"explicit",
+                            reason: "explicit",
                         },
                         explicit: false,
                         external_urls: {},
@@ -231,19 +229,200 @@ describe("browseSlice async actions", () => {
                 ]
             }
         }
-
-        test('browse.fulfilled have to set items, and change loading state', () =>{
-            const endState = browseReducer(startState, browse.fulfilled(response,'request-id', browsePayload))
-            expect(endState.items.tracks?.items[0].uri).toBe(response.tracks?.items[0].uri)
-            expect(endState.items.tracks?.items[0].name).toBe(response.tracks?.items[0].name)
-            expect(endState.items.tracks?.items[0].id).toBe(response.tracks?.items[0].id)
+        test('browse.fulfilled have to set items, and change loading state', () => {
+            const endState = browseReducer(startState, browse.fulfilled(response, 'request-id', browsePayload))
+            expect(endState.items.tracks).toEqual(response.tracks)
             expect(endState.loading).toBe('succeeded' as RequestStatuses)
         })
-
-        test('browse.pending have to change loading state',()=>{
-            const endState = browseReducer(startState, browse.pending('request-id',browsePayload))
+        test('browse.pending have to change loading state', () => {
+            const endState = browseReducer(startState, browse.pending('request-id', browsePayload))
             expect(endState.loading).toBe('loading' as RequestStatuses)
         })
     });
+    test("fetchNewPortion have to add items to exist items", () => {
+        const modifiedState: BrowseStateType = {
+            ...startState,
+            activeTab: 'tracks',
+            items: {
+                tracks: {
+                    href: 'href',
+                    next: 'next',
+                    limit: 1000,
+                    offset: 0,
+                    previous: 'prev',
+                    total: 89888,
+                    items: [
+                        {
+                            external_ids: {},
+                            available_markets: [],
+                            disc_number: 9,
+                            uri: 'usr',
+                            href: 'trackhref',
+                            type: 'track',
+                            restrictions: {
+                                reason: "explicit",
+                            },
+                            explicit: false,
+                            external_urls: {},
+                            duration_ms: 2838923,
+                            is_local: false,
+
+                            is_playable: true,
+                            track_number: 33,
+                            linked_from: {
+                                href: 'https://href',
+                                uri: 'https://href',
+                                type: 'track',
+                                external_urls: {},
+                                id: 'sdkfja;sldfkjas'
+                            },
+                            preview_url: 'sfsafsdf',
+                            id: "1234567890abcdef",
+                            name: "Track Name",
+                            artists: [
+                                {
+                                    id: "artist-id",
+                                    name: "Artist Name",
+                                    type: 'artist',
+                                    external_urls: {},
+                                    href: 'https://href',
+                                    uri: 'https://href',
+                                }
+                            ],
+                            album: {
+                                restrictions: {
+                                    reason: 'explicit'
+                                },
+                                album_type: "album",
+                                artists: [
+                                    {
+                                        id: "artist-id",
+                                        name: "Artist Name",
+                                        href: "https://api.spotify.com/v1/artists/artist-id",
+                                        external_urls: {
+                                            spotify: "https://open.spotify.com/artist/artist-id"
+                                        },
+                                        uri: 'swss',
+                                        type: 'artist'
+                                    }
+                                ],
+                                available_markets: ["US", "CA", "GB"],
+                                external_urls: {
+                                    spotify: "https://open.spotify.com/album/album-id",
+                                },
+                                href: "https://api.spotify.com/v1/albums/album-id",
+                                id: "album-id",
+                                images: [
+                                    {
+                                        url: "https://link-to-image.jpg",
+                                        height: 300,
+                                        width: 300
+                                    }
+                                ],
+                                name: "Album Name",
+                                release_date: "2025-01-01",
+                                release_date_precision: "day",
+                                total_tracks: 10,
+                                type: "album",
+                                uri: "spotify:album:album-id",
+                            },
+                            popularity: 80,
+                        }
+                    ]
+                }
+            }
+        }
+        const newPortion: SearchResult = {
+            tracks: {
+                href: 'href',
+                next: 'next',
+                limit: 1000,
+                offset: 0,
+                previous: 'prev',
+                total: 89888,
+                items: [
+                    {
+                        external_ids: {},
+                        available_markets: [],
+                        disc_number: 9,
+                        uri: 'usr',
+                        href: 'trackhref',
+                        type: 'track',
+                        restrictions: {
+                            reason: "explicit",
+                        },
+                        explicit: false,
+                        external_urls: {},
+                        duration_ms: 2838923,
+                        is_local: false,
+
+                        is_playable: true,
+                        track_number: 33,
+                        linked_from: {
+                            href: 'https://href',
+                            uri: 'https://href',
+                            type: 'track',
+                            external_urls: {},
+                            id: 'sdkfja;sldfkjas'
+                        },
+                        preview_url: 'sfsafsdf',
+                        id: "1234567890abcdef",
+                        name: "Track 1",
+                        artists: [
+                            {
+                                id: "artist-id",
+                                name: "Artist Name",
+                                type: 'artist',
+                                external_urls: {},
+                                href: 'https://href',
+                                uri: 'https://href',
+                            }
+                        ],
+                        album: {
+                            restrictions: {
+                                reason: 'explicit'
+                            },
+                            album_type: "album",
+                            artists: [
+                                {
+                                    id: "artist-id",
+                                    name: "Artist Name",
+                                    href: "https://api.spotify.com/v1/artists/artist-id",
+                                    external_urls: {
+                                        spotify: "https://open.spotify.com/artist/artist-id"
+                                    },
+                                    uri: 'swss',
+                                    type: 'artist'
+                                }
+                            ],
+                            available_markets: ["US", "CA", "GB"],
+                            external_urls: {
+                                spotify: "https://open.spotify.com/album/album-id",
+                            },
+                            href: "https://api.spotify.com/v1/albums/album-id",
+                            id: "album-id",
+                            images: [
+                                {
+                                    url: "https://link-to-image.jpg",
+                                    height: 300,
+                                    width: 300
+                                }
+                            ],
+                            name: "Album Name",
+                            release_date: "2025-01-01",
+                            release_date_precision: "day",
+                            total_tracks: 10,
+                            type: "album",
+                            uri: "spotify:album:album-id",
+                        },
+                        popularity: 80,
+                    }
+                ]
+            }
+        }
+        const endState = browseReducer(modifiedState, fetchNewPortion.fulfilled(newPortion, 'request-id'))
+        expect(endState.items.tracks?.items[0]).equal(modifiedState.items.tracks?.items[0])
+        expect(endState.items.tracks?.items[1]).equal(newPortion.tracks?.items[0])
+    })
     //todo: fetchNewPortion tests
 });
