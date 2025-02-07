1bc323c1f559d43fc4a5092af284a22845832fcc
gqtpe <work.temirtas@gmail.com>
2025-02-07 22:26:52 +0500

    Spotify v1.2(test librarySlice): add tests for setFilter action
                                    This follows a structured format:
    
                                    test → Indicates this commit is related to tests.
                                    librarySlice → Specifies the relevant module or feature.
                                    add tests for setFilter action → Clearly states what the commit does.

---
+++ src/features/Library/tests/librarySlice.test.ts
@@ -0,0 +1,33 @@
+import {LibraryFilterType, LibraryStateType} from "../userLibrarySlice.ts";
+import {beforeEach, describe, expect, test} from "vitest";
+import {userLibraryActions, userLibraryReducer} from "../index.ts";
+
+
+let startState: LibraryStateType;
+
+const {setFilter} = userLibraryActions;
+
+beforeEach(() => {
+    startState = {
+        items: [],
+        filter: 'all'
+    }
+})
+
+
+describe('librarySlice synchronous action setFilter', () => {
+    const modifiedState: LibraryStateType = {
+        ...startState,
+        filter: 'album'
+    }
+    test('should set filter to playlists', () => {
+        const payload: LibraryFilterType = 'playlist'
+        const endState = userLibraryReducer(startState, setFilter(payload))
+        expect(endState.filter).toBe(payload)
+    })
+    test('should set filter to all', () => {
+        const payload: LibraryFilterType = 'all'
+        const endState = userLibraryReducer(modifiedState, setFilter(payload))
+        expect(endState.filter).toBe(payload)
+    })
+})
