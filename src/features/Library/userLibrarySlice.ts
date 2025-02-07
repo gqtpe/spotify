fd6aa214730cdeac4c2ce08dccabb924342918e9
gqtpe <work.temirtas@gmail.com>
2025-02-07 22:28:04 +0500

    Spotify v1.2(test librarySlice):added librarySlice state type

--- src/features/Library/userLibrarySlice.ts
+++ src/features/Library/userLibrarySlice.ts
@@ -2,16 +2,19 @@ import {createSlice} from "@reduxjs/toolkit";
 import type {Album} from "../../api/types/album.ts";
 import {SimplifiedPlaylist} from "../../api/types/playlist.ts";
 import {fetchUserLibrary} from "./libraryThunks.ts";
-
-type Filter = 'all' | 'playlist' | 'album'
+export type LibraryStateType = {
+    items: Array<SimplifiedPlaylist | Album>
+    filter: LibraryFilterType
+}
+export type LibraryFilterType = 'all' | 'playlist' | 'album'
 const slice = createSlice({
     name: 'userLibrary',
     initialState: {
-        items: [] as Array<SimplifiedPlaylist | Album>,
-        filter: 'all' as Filter
-    },
+        items: [],
+        filter: 'all'
+    } as LibraryStateType,
     reducers: {
-        setFilter(state, action: { payload: Filter }) {
+        setFilter(state, action: { payload: LibraryFilterType }) {
             state.filter = action.payload
         },
     },
