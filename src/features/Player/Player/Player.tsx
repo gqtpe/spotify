f05931172f5426fa061d8c4986d034ff2cfac6db
gqtpe <work.temirtas@gmail.com>
2025-02-10 23:27:18 +0500

    Spotify v1.2(player): now more clear

--- src/features/Player/Player/Player.tsx
+++ src/features/Player/Player/Player.tsx
@@ -1,59 +1,19 @@
 import {FC, memo} from "react";
 import styles from "../Footer.module.scss";
-import IconButton from "../../../common/components/IconButton/IconButton.tsx";
-import {FaPause, FaPlay, FaShuffle} from "react-icons/fa6";
-import {FaStepBackward, FaStepForward} from "react-icons/fa";
-import {playerSelectors, ProgressBar, Repeat, usePlayerActions} from "../index.ts";
-import {useAppSelector} from "../../Application/hooks";
+import {Actions, ProgressBar} from "../index.ts";
+import {useProgressBar} from "../../Application/hooks";
 
 
 const Player: FC = () => {
-    const activeDevice = useAppSelector(playerSelectors.selectActiveDevice)
-    const shuffleState = useAppSelector(playerSelectors.selectShuffleState)
-    const is_playing = useAppSelector(playerSelectors.selectIsPlaying)
-    const item = useAppSelector(playerSelectors.selectPlaybackItem)
-    const repeatState = useAppSelector(playerSelectors.selectRepeatState)
-    const playbackLoading = useAppSelector(playerSelectors.selectPlaybackLoading)
-    const progress = useAppSelector(playerSelectors.selectProgress)
-
-    const {togglePlay, shuffle, next, prev, repeat,seekPosition,fetchCurrentlyPlaying} = usePlayerActions(activeDevice?activeDevice.id:null, shuffleState, is_playing, repeatState)
-
-
+    const {onSeek, onSeekEnd, progress, duration} = useProgressBar()
     return <div className={[styles.footer__player, styles.player].join(' ')}>
-        <div className={styles.player__actions}>
-            <IconButton variant="icon" className={styles.shuffle} onClick={shuffle} disabled={!activeDevice}>
-                {shuffleState ? <FaShuffle className={styles.active}/> : <FaShuffle/>}
-            </IconButton>
-            <IconButton variant="icon" onClick={prev} disabled={!activeDevice}>
-                <FaStepBackward/>
-            </IconButton>
-            <IconButton variant="outlined" className={styles.play} onClick={togglePlay} disabled={!activeDevice}>
-                {is_playing ? <FaPause/> : <FaPlay/>}
-            </IconButton>
-            <IconButton variant="icon" onClick={next} disabled={!activeDevice}>
-                <FaStepForward/>
-            </IconButton>
-            <Repeat variant={repeatState} onClick={repeat} disabled={!activeDevice}/>
-        </div>
-        {(playbackLoading === 'succeeded') &&
-            <ProgressBar
-                progress={progress!}
-                duration={item!.duration_ms}
-                onSeek={seekPosition}
-                onSeekEnd={fetchCurrentlyPlaying}
-                loading={false}
-                isPlaying={is_playing}
-            />}
-        {playbackLoading !== 'succeeded' &&
-            <ProgressBar
-                progress={0}
-                duration={0}
-                onSeek={seekPosition}
-                loading={true}
-                isPlaying={false}
-            />
-        }
-
+        <Actions/>
+        <ProgressBar
+            onSeek={onSeek}
+            onSeekEnd={onSeekEnd}
+            progress={progress}
+            duration={duration}
+        />
     </div>
 }
 export default memo(Player);
