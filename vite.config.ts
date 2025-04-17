09f669e1b8727f425801526dc18c6b695b2ca88b
gqtpe <work.temirtas@gmail.com>
2025-04-17 10:34:21 +0500

    Spotify v1.21(refactoring):added import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+import {AliasOptions, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//@ts-ignore
+import path from "path";
+
+//@ts-ignore
+const root = path.resolve(__dirname, "src");
+const common = path.resolve(__dirname, "src/common");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": root,
+      "@common":common,
+    } as AliasOptions,
+  },
   plugins: [react()],
   server: {port: 3000},
   css: {
